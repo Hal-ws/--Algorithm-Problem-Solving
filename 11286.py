b59318b57d2ab946975e29a63513f7f8e0af5086
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2020-09-22 15:47:10 +0900

    Create 11286.py

---
+++ 11286.py
@@ -0,0 +1,37 @@
+import sys
+from queue import PriorityQueue
+def main():
+    plusque, minusque, cntPlus, cntMinus = PriorityQueue(), PriorityQueue(), 0, 0
+    N = int(sys.stdin.readline())
+    for i in range(N):
+        query = int(sys.stdin.readline())
+        if query == 0:
+            if cntPlus > 0 and cntMinus > 0:
+                plusVal, minusVal = plusque.get(), minusque.get()
+                if minusVal <= plusVal:
+                    print(minusVal * (-1))
+                    cntMinus -= 1
+                    plusque.put(plusVal)
+                else:
+                    print(plusVal)
+                    cntPlus -= 1
+                    minusque.put(minusVal)
+            elif cntPlus > 0 and cntMinus == 0:
+                print(plusque.get())
+                cntPlus -= 1
+            elif cntPlus == 0 and cntMinus > 0:
+                print(minusque.get())
+                cntMinus -= 1
+            else:
+                print(0)
+        else:
+            if query < 0:
+                minusque.put(query * (-1))
+                cntMinus += 1
+            else:
+                plusque.put(query)
+                cntPlus += 1
+
+
+if __name__ == "__main__":
+    main()
