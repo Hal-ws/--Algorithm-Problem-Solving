f638d5ea0372af06b41d3f1e9edb4041eea8a4a0
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2020-10-15 17:24:42 +0900

    Update 14002.py

--- 14002.py
+++ 14002.py
@@ -2,21 +2,21 @@ def main():
     N = int(input())
     nums = list(map(int, input().split()))
     dp = [[nums[0], 1]]
+    ansList = [nums[0]]
     for i in range(1, N):
-        dp.append(getVal(dp, nums, i))
+        dp.append(getVal(dp, nums, ansList, i))
     print(dp)
 
 
-def getVal(dp, nums, idx):
+def getVal(dp, nums, ansList, idx):
     findflag = 0
     for i in range(idx - 1, -1, -1):
         if dp[i][0] < nums[idx]:
-            num = dp[i][0]
             length = dp[i][1] + 1
             findflag = 1
             break
     if findflag:
-        return [num, length]
+        return [nums[idx], length]
     else:
         return [nums[idx], 1]
 
