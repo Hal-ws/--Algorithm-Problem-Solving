cc9308383af77d19af2d0399a2471a0f65eb0af8
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2020-10-15 22:54:47 +0900

    Update 14002.py

--- 14002.py
+++ 14002.py
@@ -5,18 +5,25 @@ def main():
     ansList = [[nums[0]]] + [0] * (N - 1)
     for i in range(1, N):
         dp.append(getVal(dp, nums, ansList, i))
-    print(dp)
-    print(ansList)
+    la = len(ansList[N - 1])
+    print(la)
+    for i in range(la):
+        print(ansList[N - 1][i], end = ' ')
 
 
 def getVal(dp, nums, ansList, idx):
-    lastPoint = [0, 0] ## LAST 포인트가 되는 길이, index 저장
+    lastPoint = [0, 0]  ## LAST 포인트가 되는 길이, index 저장
     for i in range(idx - 1, -1, -1):
         if dp[i][0] < nums[idx]:
-            length = dp[i][1] + 1
             temp = dp[i][1]
             if temp > lastPoint[0]:
                 lastPoint = [temp, i]
+    if lastPoint == [0, 0]:
+        ansList[idx] = [nums[idx]]
+        return [nums[idx], 1]
+    else:
+        ansList[idx] = ansList[lastPoint[1]] + [nums[idx]]
+        return [nums[idx], dp[lastPoint[1]][1] + 1]
 
 
 if __name__ == "__main__":
