adf70451137575e823b311f07b32fdea888b5049
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2020-10-28 17:01:43 +0900

    Update 14226.py

--- 14226.py
+++ 14226.py
@@ -1,36 +1,38 @@
-from collections import deque
-
-
 def main():
     S = int(input())
-    q = deque()
-    q.append([1, 0, 0]) ## 이모티콘 개수, 클립보드 저장, 시간
-    visitChk = [0] * (2 * S + 1)
-    visitChk[1] = 1
-    while 1:
-        if q[0][0] > 2:
-            if visitChk[q[0][0] - 1] == 0:
-                visitChk[q[0][0] - 1] = 1
-                q.append([q[0][0] - 1, q[0][1], q[0][2] + 1])
-                if q[0][0] - 1 == S:
-                    ans = q[0][2] + 1
+    timeresult = [[[2, 1]], [[3, 1]]] ## 2, 3초에 생기는 경우의수
+    time = 4
+    if S == 2:
+        print(2)
+    else:
+        endflag = 0
+        while 1:
+            first = timeresult[0]
+            second = timeresult[1]
+            temp = []
+            l1 = len(first)
+            l2 = len(second)
+            for i in range(l1):
+                if first[i][0] * 2 == S:
+                    endflag = 1
                     break
-        if q[0][1] + q[0][0] <= 2 * S:
-            if visitChk[q[0][1] + q[0][0]] == 0:
-                visitChk[q[0][0] + q[0][1]] = 1
-                q.append([q[0][0] + q[0][1], q[0][1], q[0][2] + 1])
-                if q[0][0] + q[0][1] == S:
-                    ans = q[0][2] + 1
+                temp.append([first[i][0] * 2, first[i][0]])
+            for i in range(l2):
+                if second[i][0] + second[i][1] == S:
+                    endflag = 1
                     break
-        if q[0][0] * 2 <= 2 * S:
-            if visitChk[q[0][0] * 2] == 0:
-                visitChk[q[0][0] * 2] = 1
-                q.append([q[0][0] * 2, q[0][0], q[0][2] + 2])
-                if q[0][0] * 2 == S:
-                    ans = q[0][2] + 2
-                    break
-        del q[0]
-    print(ans)
+                temp.append([second[i][0] + second[i][1], second[i][1]])
+                if second[i][0] - 1 > 4:
+                    if second[i][0] - 1 == S:
+                        endflag = 1
+                        break
+                    temp.append([second[i][0] - 1, second[i][1]])
+            if endflag:
+                break
+            timeresult[0] = timeresult[1]
+            timeresult[1] = temp
+            time += 1
+        print(time)
 
 
 if __name__ == "__main__":
