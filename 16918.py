cf3e7a050565401f40afe1950fe8eed97cdf59a4
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2020-10-30 00:34:17 +0900

    Update 16918.py

--- 16918.py
+++ 16918.py
@@ -11,23 +11,60 @@ def main():
             if blocks[i][j] == "O":
                 blocks[i][j] = 2
             else:
+                blocks[i][j] = -1 # 폭탄 없는건 -1
+    for i in range(R):
+        for j in range(C):
+            if blocks[i][j] != 2:
                 blocks[i][j] = 0
-    time = 2
-    while time <= N:
-        print('one: %s' %one)
-        print('two: %s' %two)
-        print('three: %s' %three)
-        if time % 2 == 1:
-            
-        else:
-
-        print('time: %s' %time)
+    time = 3
+    if N > 1:
+        while time <= N:
+            if time % 2 == 0:
+                setbomb(blocks, R, C)
+            else:
+                explosion(blocks, R, C)
+            time += 1
         for i in range(R):
             for j in range(C):
-                print(blocks[i][j], end='')
+                if blocks[i][j] != -1:
+                    print('O', end='')
+                else:
+                    print('.', end='')
             print()
-        time += 1
-        print('--------------------------------------')
+    else:
+        for i in range(R):
+            for j in range(C):
+                if blocks[i][j] == 2:
+                    print('O', end='')
+                else:
+                    print('.', end='')
+            print()
+
+
+def explosion(blocks, R, C):
+    for i in range(R):
+        for j in range(C):
+            if blocks[i][j] != -1:
+                blocks[i][j] += 1
+            if blocks[i][j] == 3: # 설치한지 3초지난 폭탄 발견
+                blocks[i][j] = -1 # 폭발해서 폭탄 없어짐
+                if i > 0 and blocks[i - 1][j] != 2:
+                    blocks[i - 1][j] = -1
+                if i < R - 1 and blocks[i + 1][j] != 2:
+                    blocks[i + 1][j] = -1
+                if j > 0 and blocks[i][j - 1] != 2:
+                    blocks[i][j - 1] = -1
+                if j < C - 1 and blocks[i][j + 1] != 2:
+                    blocks[i][j + 1] = -1
+
+
+def setbomb(blocks, R, C):
+    for i in range(R):
+        for j in range(C):
+            if blocks[i][j] != 0:
+                blocks[i][j] += 1
+            else:
+                blocks[i][j] = 1
 
 
 if __name__ == "__main__":
