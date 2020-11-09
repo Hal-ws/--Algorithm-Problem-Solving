fd91c99f47df0f076796d5ce86af16462d68a27a
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2020-11-09 13:48:06 +0900

    Create 17618.py

---
+++ 17618.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+
+def main():
+    N, K = map(int, input().split())
+    q = deque()
+    visitChk = [None] * 200001
+    visitChk[N] = 0
+    if N == K:
+        print(0)
+        return 0
+    if N < 100000:
+        visitChk[2 * N] = 1
+        q.append([2 * N, 0])
+        visitChk[N + 1] = 1
+        q.append([N + 1, 1])
+    if N > 0:
+        visitChk[N - 1] = 1
+        q.append([N - 1, 1])
+    print(bfs(q, K, visitChk))
+
+
+def bfs(q, K, visitChk):
+    while len(q) > 0:
+        if q[0][0] <= 100000 and (visitChk[q[0][0] * 2] == None or visitChk[q[0][0] * 2] > q[0][1]):
+                q.append([q[0][0] * 2, q[0][1]])
+                visitChk[q[0][0] * 2] = q[0][1]
+        if q[0][0] < 200000 and (visitChk[q[0][0] + 1] == None or visitChk[q[0][0] + 1] > q[0][1] + 1):
+            q.append([q[0][0] + 1, q[0][1] + 1])
+            visitChk[q[0][0] + 1] = q[0][1] + 1
+        if q[0][0] > 0 and (visitChk[q[0][0] - 1] == None or visitChk[q[0][0] - 1] > q[0][1] + 1):
+            q.append([q[0][0] - 1, q[0][1] + 1])
+            visitChk[q[0][0] - 1] = q[0][1] + 1
+        if q[0][0] == K:
+            return q[0][1]
+        del q[0]
+
+
+if __name__ == "__main__":
+    main()
