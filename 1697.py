0c36ed07e88738f7877ddac449efd1bb6f3f9409
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2020-11-09 10:21:04 +0900

    Update 1697.py

--- 1697.py
+++ 1697.py
@@ -1,49 +1,39 @@
-import sys
 from collections import deque
 
 
 def main():
-    N, K = map(int, sys.stdin.readline().split())
+    N, K = map(int, input().split())
     q = deque()
-    q.append(['m', N - 1])
-    q.append(['p', N + 1])
-    q.append(['d', 2 * N])
-    print(bfs(q, N, K))
-
-def bfs(q, N, K):
+    visitChk = [0] * 200001
+    visitChk[N] = 1
     if N == K:
+        print(0)
         return 0
-    if q[0][1] == K or q[1][1] == K or q[2][1] == K:
-        return 1
-    lb, la = 3, 3
-    seconds = 1
-    while 1:
-        for i in range(lb):
-            if q[i][0] == 'm':
-                q.append(['m', q[i][1] - 1])
-                q.append(['d', q[i][1] * 2])
-                la += 2
-                if q[la - 2][1] == K or q[la - 1][1] == K:
-                    return seconds + 1
-            elif q[i][0] == 'p':
-                q.append(['p', q[i][1] + 1])
-                q.append(['d', q[i][1] * 2])
-                la += 2
-                if q[la - 2][1] == K or q[la - 1][1] == K:
-                    return seconds + 1
-            else:
-                q.append(['m', q[i][1] - 1])
-                q.append(['p', q[i][1] + 1])
-                q.append(['d', q[i][1] * 2])
-                la += 3
-                if q[la - 3][1] == K or q[la - 2][1] == K or q[la - 1][1] == K:
-                    return seconds + 1
-        for i in range(lb):
-            q.popleft()
-            la -= 1
-        lb = la
-        seconds += 1
-    print(seconds)
+    if N > 0:
+        visitChk[N - 1] = 1
+        q.append([N - 1, 1])
+    if N < 100000:
+        visitChk[N + 1] = 1
+        q.append([N + 1, 1])
+        visitChk[2 * N] = 1
+        q.append([2 * N, 1])
+    print(bfs(q, K, visitChk))
+
+def bfs(q, K, visitChk):
+    while len(q) > 0:
+        if q[0][0] > 0 and visitChk[q[0][0] - 1] == 0:
+            q.append([q[0][0] - 1, q[0][1] + 1])
+            visitChk[q[0][0] - 1] = 1
+        if q[0][0] < 200000 and visitChk[q[0][0] + 1] == 0:
+            q.append([q[0][0] + 1, q[0][1] + 1])
+            visitChk[q[0][0] + 1] = 1
+        if q[0][0] <= 100000 and visitChk[q[0][0] * 2] == 0:
+            q.append([q[0][0] * 2, q[0][1] + 1])
+            visitChk[q[0][0] * 2] = 1
+        if q[0][0] == K:
+            return q[0][1]
+        del q[0]
+
 
 if __name__ == "__main__":
     main()
