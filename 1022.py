3c369d384fd76ffe06831c05d9fd99c6a0a2564c
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2020-11-11 00:33:56 +0900

    Update 1022.py

--- 1022.py
+++ 1022.py
@@ -1,38 +1,38 @@
 def main():
     r1, c1, r2, c2 = map(int, input().split())
-    M = max(abs(r1), abs(r2), abs(c1), abs(c2))
-    paper = [[0 for i in range(2 * M + 1)] for j in range(2 * M + 1)]
-    n = 1
-    for i in range(M + 1):
-        paper[M + i][M + i] = pow((2 * i + 1), 2)
-        paper = cycle(paper, M + i)
-    maxlen = len(str(paper[2 * M][2 * M]))
+    plate = [[0 for i in range(c2 - c1 + 1)] for j in range(r2 - r1 + 1)]
+    for i in range(r1, r2 + 1):
+        for j in range(c1, c2 + 1):
+            plate[i - r1][j - c1] = getnum(i, j)
+    maxVal = 0
+    for i in range(r2 - r1 + 1):
+        if max(plate[i]) >= maxVal:
+            maxVal = max(plate[i])
+    maxLen = len(str(maxVal))
     for i in range(r2 - r1 + 1):
         for j in range(c2 - c1 + 1):
-            l = len(str(paper[i][j]))
-            print(' ' * (maxlen - l) + str(paper[i][j]), end = ' ')
+            blank = ' ' * (maxLen - len(str(plate[i][j])))
+            print('%s%s' %(blank, plate[i][j]), end=' ')
         print()
 
-def cycle(paper, point):
-    val = paper[point][point] - 1
-    i, j = point, point
-    for k in range((point - 3) * 2):
-        j -= 1
-        paper[i][j] = val
-        val -= 1
-    for k in range((point - 3) * 2):
-        i -= 1
-        paper[i][j] = val
-        val -= 1
-    for k in range((point - 3) * 2):
-        j += 1
-        paper[i][j] = val
-        val -= 1
-    for k in range((point - 3) * 2 - 1):
-        i += 1
-        paper[i][j] = val
-        val -= 1
-    return paper
+
+def getnum(i, j):
+    layer = max(abs(i), abs(j))
+    maxVal = pow(2 * layer + 1, 2)
+    distance = 0
+    if i == layer and j == layer:
+        distance = 0
+    elif i == (-1) * layer and j == (-1) * layer:
+        distance = layer * 4
+    elif i == layer or j == (-1) * layer: # 가까운지역
+        distance += layer - i
+        distance += layer - j
+    else:
+        distance += layer * 4
+        distance += layer + i
+        distance += layer + j
+    return maxVal - distance
+
 
 if __name__ == "__main__":
     main()
