59fceffab188f8633b2f436e0a9d642403ca2656
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2020-11-18 23:51:26 +0900

    Update 2251.py

--- 2251.py
+++ 2251.py
@@ -10,37 +10,19 @@ def main():
     while len(q) > 0:
         if q[0][0] > 0: # A를 붓는다
             if q[0][1] < B: # B에 붓는다
-                temp = pour(0, 1, 2, q, B)
-                if chk[temp[0]][temp[1]] == -1:
-                    chk[temp[0]][temp[1]] = temp[2]
-                    q.append(temp)
+                pour(0, 1, 2, q, B, chk)
             if q[0][2] < C: # C에 붓는다
-                temp = pour(0, 2, 1, q, C)
-                if chk[temp[0]][temp[1]] == -1:
-                    chk[temp[0]][temp[1]] = temp[2]
-                    q.append(temp)
+                pour(0, 2, 1, q, C, chk)
         if q[0][1] > 0: # B를 붓는다
             if q[0][0] < A:
-                temp = pour(1, 0, 2, q, A)
-                if chk[temp[0]][temp[1]] == -1:
-                    chk[temp[0]][temp[1]] = temp[2]
-                    q.append(temp)
+                pour(1, 0, 2, q, A, chk)
             if q[0][2] < C:
-                temp = pour(1, 2, 0, q, C)
-                if chk[temp[0]][temp[1]] == -1:
-                    chk[temp[0]][temp[1]] = temp[2]
-                    q.append(temp)
+                pour(1, 2, 0, q, C, chk)
         if q[0][2] > 0:
             if q[0][0] < A:
-                temp = pour(2, 0, 1, q, A)
-                if chk[temp[0]][temp[1]] == -1:
-                    chk[temp[0]][temp[1]] = temp[2]
-                    q.append(temp)
+                pour(2, 0, 1, q, A, chk)
             if q[0][1] < B:
-                temp = pour(2, 1, 0, q, B)
-                if chk[temp[0]][temp[1]] == -1:
-                    chk[temp[0]][temp[1]] = temp[2]
-                    q.append(temp)
+                pour(2, 1, 0, q, B, chk)
         q.popleft()
     ansList = sorted(chk[0])
     for i in range(B + 1):
@@ -49,7 +31,7 @@ def main():
     return 0
 
 
-def pour(s, e, n, q, M): ## s index에 있는걸 e index 로 붓는다. n은 상관없는 index M: e index가 저장가능한 용량
+def pour(s, e, n, q, M, chk): ## s index에 있는걸 e index 로 붓는다. n은 상관없는 index M: e index가 저장가능한 용량
     temp = [0, 0, 0]
     if q[0][s] < M - q[0][e]: # 있는거 다 부어버릴수 있음
         temp[s] = 0
@@ -58,6 +40,9 @@ def pour(s, e, n, q, M): ## s index에 있는걸 e index 로 붓는다. n은 상
         temp[e] = M
         temp[s] = q[0][s] - (M - q[0][e])
     temp[n] = q[0][n]
+    if chk[temp[0]][temp[1]] == -1:
+        chk[temp[0]][temp[1]] = temp[2]
+        q.append(temp)
     return temp
 
 
