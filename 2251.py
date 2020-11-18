7a152e5f91936c745b3e9e7a9eb90dd25c650b9c
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2020-11-18 23:49:32 +0900

    Update 2251.py

--- 2251.py
+++ 2251.py
@@ -3,62 +3,62 @@ from collections import deque
 
 def main():
     A, B, C = map(int, input().split())
-    water = [0, 0, C]
     q = deque()
     q.append([0, 0, C])
-    chk = [[0] for i in range(B + 1) for j in range(A + 1)]
+    chk = [[-1 for i in range(B + 1)] for j in range(A + 1)]
     chk[0][0] = C
     while len(q) > 0:
-        if q[0][0] > 0:
-            if q[0][1] < B: # B에 부음
-                if q[0][0] > B - q[0][1]: # B에 다 붓고도 남음
-                    if chk[q[0][0] - B + q[0][1]][B] == 0:
-                        chk[q[0][0] - B + q[0][1]][B] = q[0][2]
-                        q.append(q[0][0] - B + q[0][1], B, q[0][2])
-                else: #남는거 다부음
-                    if chk[0][q[0][1] + q[0][0]] == 0:
-                        chk[0][q[0][1] + q[0][0]] = q[0][2]
-                        q.append([0, q[0][1] + q[0][0], q[0][2]])
-            if q[0][2] < C: # C에부음
-                if q[0][0] > C - q[0][2]:
-                    if chk[q[0][0] - C + q[0][2]][q[0][1]] == 0:
-                        chk[q[0][0] - C + q[0][2]][q[0][1]] = C
-                        q.append([q[0][0] - C + q[0][2], q[0][1], C])
-                else:
-                    if chk[0][q[0][1]] == 0:
-                        chk[0][q[0][1]] = q[0][2] + q[0][0]
-                        q.append([0, q[0][1], q[0][2] + q[0][0]])
-        if q[0][1] > 0:
+        if q[0][0] > 0: # A를 붓는다
+            if q[0][1] < B: # B에 붓는다
+                temp = pour(0, 1, 2, q, B)
+                if chk[temp[0]][temp[1]] == -1:
+                    chk[temp[0]][temp[1]] = temp[2]
+                    q.append(temp)
+            if q[0][2] < C: # C에 붓는다
+                temp = pour(0, 2, 1, q, C)
+                if chk[temp[0]][temp[1]] == -1:
+                    chk[temp[0]][temp[1]] = temp[2]
+                    q.append(temp)
+        if q[0][1] > 0: # B를 붓는다
             if q[0][0] < A:
-                if q[0][1] > A - q[0][0]:
-                    if chk[A][q[0][1] - A + q[0][0]] == 0:
-                        chk[A][q[0][1] - A + q[0][0]] = q[0][2]
-                        q.append([A, q[0][1] - A + q[0][0], q[0][2]])
-                else:
-                    if chk[q[0][0] + q[0][1]][0] == 0:
-                        chk[q[0][0] + q[0][1]][0] = q[0][2]
-                        q.append([q[0][0] + q[0][1], 0, q[0][2]])
+                temp = pour(1, 0, 2, q, A)
+                if chk[temp[0]][temp[1]] == -1:
+                    chk[temp[0]][temp[1]] = temp[2]
+                    q.append(temp)
             if q[0][2] < C:
-                if q[0][1] > C - q[0][2]:
-                    if chk[q[0][0]][q[0][1] - C + q[0][2]] == 0:
-                        chk[q[0][0]][q[0][1] - C + q[0][2]] = C
-                        q.append([q[0][0], q[0][1] - C + q[0][2], C])
-                else:
-                    if chk[q[0][0]][0] == 0:
-                        chk[q[0][0]][0] = q[0][2] + q[0][1]
-                        q.append([q[0][0], 0, q[0][2] + q[0][1]])
+                temp = pour(1, 2, 0, q, C)
+                if chk[temp[0]][temp[1]] == -1:
+                    chk[temp[0]][temp[1]] = temp[2]
+                    q.append(temp)
         if q[0][2] > 0:
             if q[0][0] < A:
-                if q[0][2] > A - q[0][0]:
-                    if chk[A][q[0][1]] == 0:
-                        chk[A][q[0][1]] = q[0][2] - A + q[0][0]
-                        q.append([A, q[0][1], q[0][2] - A + q[0][0]])
-                else:
-                    if chk[q[0][0] + q[0][2]][q[0][1]] == 0:
-                        chk[q[0][0] + q[0][2]][q[0][1]] = 0
-                        q.append([q[0][0] + q[0][2], q[0][1], 0])
+                temp = pour(2, 0, 1, q, A)
+                if chk[temp[0]][temp[1]] == -1:
+                    chk[temp[0]][temp[1]] = temp[2]
+                    q.append(temp)
+            if q[0][1] < B:
+                temp = pour(2, 1, 0, q, B)
+                if chk[temp[0]][temp[1]] == -1:
+                    chk[temp[0]][temp[1]] = temp[2]
+                    q.append(temp)
         q.popleft()
-    print(chk[0])
+    ansList = sorted(chk[0])
+    for i in range(B + 1):
+        if ansList[i] != -1:
+            print(ansList[i], end=' ')
+    return 0
+
+
+def pour(s, e, n, q, M): ## s index에 있는걸 e index 로 붓는다. n은 상관없는 index M: e index가 저장가능한 용량
+    temp = [0, 0, 0]
+    if q[0][s] < M - q[0][e]: # 있는거 다 부어버릴수 있음
+        temp[s] = 0
+        temp[e] = q[0][e] + q[0][s]
+    else: # 있는거 다부으면 넘침
+        temp[e] = M
+        temp[s] = q[0][s] - (M - q[0][e])
+    temp[n] = q[0][n]
+    return temp
 
 
 if __name__ == "__main__":
