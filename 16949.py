90cc0cebac19fc261ca7d267502ba904b63a37c1
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2020-11-23 09:56:05 +0900

    Update 16949.py

--- 16949.py
+++ 16949.py
@@ -1,54 +1,65 @@
 import sys
-from collections import deque
-from copy import deepcopy
+from _collections import deque
+
 
 def main():
     N, M = map(int, sys.stdin.readline().split())
-    room = []
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    areas = [0, 0] #각각 공간의 크기를 2번 공간부터 지정함.
-    areaidx = 2
+    board = []
     for i in range(N):
-        room.append(list(map(int, sys.stdin.readline()[:M])))
-    ans = deepcopy(room)
+        board.append(list(map(int, sys.stdin.readline()[:M])))
+    chk = [[0 for i in range(M)] for j in range(N)]
+    newboard = [[[0, 0] for i in range(M)] for j in range(N)]
+    idx = 1
     for i in range(N):
         for j in range(M):
-            if room[i][j] == 0:
-                q = deque([[i, j]])
-                room[i][j] = areaidx
-                temparea = 1
-                while len(q) > 0:
-                    for k in range(4):
-                        y, x = q[0][0] + dy[k], q[0][1] + dx[k]
-                        if 0 <= y < N and 0 <= x < M and room[y][x] == 0:
-                            room[y][x] = areaidx
-                            q.append([y, x])
-                            temparea += 1
-                    q.popleft()
-                areas.append(temparea)
-                areaidx += 1
+            if board[i][j] == 0 and chk[i][j] == 0:
+                bfs(board, newboard, chk, idx, [i, j], N, M)
+                idx += 1
     for i in range(N):
         for j in range(M):
-            if ans[i][j] == 1:
-                ans[i][j] += getans(room, areas, i, j, areaidx, N, M)
+            if board[i][j] == 1:
+                board[i][j] = getans(newboard, [i, j], N, M) % 10
     for i in range(N):
         for j in range(M):
-            print(ans[i][j], end='')
+            print(board[i][j], end='')
         print()
+    return 0
 
 
-def getans(room, areas, y, x, areaidx, N, M):
-    visitChk = [0] * areaidx
-    poss = 0
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+def bfs(board, newboard, chk, idx, pos, N, M):
+    q = deque()
+    q.append(pos)
+    chk[pos[0]][pos[1]] = 1
+    dy = [-1, 0, 0, 1]
+    dx = [0, 1, -1, 0]
+    areaq = [pos]
+    area = 1
+    while len(q) > 0:
+        for i in range(4):
+            y = q[0][0] + dy[i]
+            x = q[0][1] + dx[i]
+            if 0 <= y < N and 0 <= x < M and board[y][x] == 0 and chk[y][x] == 0:
+                q.append([y, x])
+                chk[y][x] = 1
+                area += 1
+        areaq.append(q.popleft())
+    la = len(areaq)
+    for i in range(la):
+        newboard[areaq[i][0]][areaq[i][1]] = [idx, area]
+
+
+def getans(newboard, pos, N, M):
+    dy = [-1, 0, 0, 1]
+    dx = [0, -1, 1, 0]
+    idxList = []
+    ans = 1
     for i in range(4):
-        if 0 <= y + dy[i] < N and 0 <= x + dx[i] < M and room[y + dy[i]][x + dx[i]] != 1:
-            if visitChk[areas[room[y + dy[i]][x + dx[i]]]] == 0:
-                visitChk[room[y + dy[i]][x + dx[i]]] = 1
-                poss += areas[room[y + dy[i]][x + dx[i]]]
-    return poss
+        y = pos[0] + dy[i]
+        x = pos[1] + dx[i]
+        if 0 <= y < N and 0 <= x < M and newboard[y][x][0] not in idxList and newboard[y][x][1] != 0:
+            idxList.append(newboard[y][x][0])
+            ans += newboard[y][x][1]
+    return ans
 
 
 if __name__ == "__main__":
