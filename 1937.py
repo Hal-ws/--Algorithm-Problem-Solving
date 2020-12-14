1b5eb1570a1d45ec6e54d9cc57afa389790f644c
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2020-12-14 13:44:15 +0900

    Update 1937.py

--- 1937.py
+++ 1937.py
@@ -1,59 +1,46 @@
 import sys
-from queue import deque
-
+sys.setrecursionlimit(10**6)
 
 def main():
     N = int(sys.stdin.readline())
-    trees = []
+    board = []
     for i in range(N):
-        trees.append(list(map(int, sys.stdin.readline().split())))
-    lifeList = [[0 for i in range(N)] for j in range(N)]
-    for i in range(N):
-        for j in range(N):
-            if lifeList[i][j] == 0:
-                bfs(trees, lifeList, [i, j], N)
-    ans = 0
+        board.append(list(map(int, sys.stdin.readline().split())))
+    chk = [[0 for j in range(N)] for i in range(N)] #수명 기록
+    maxDay = 0
     for i in range(N):
         for j in range(N):
-            if ans <= lifeList[i][j]:
-                ans = lifeList[i][j]
-    print(ans)
+            if chk[i][j] == 0:
+                dfs(board, chk, [i, j], 1, N)
+                if maxDay <= chk[i][j]:
+                    maxDay = chk[i][j]
+    print(maxDay)
 
 
-def bfs(trees, lifeList, panda, N):
-    distance = [[-1 for i in range(N)] for j in range(N)] ## -1: 아직 방문 안했음. 방문한거랑 거리를 한 행렬로 표현
-    q = deque()
-    q.append([panda[0], panda[1], 0])
-    distance[panda[0]][panda[1]] = 0
-    while 1:
-        if q[0][0] > 0:
-            if distance[q[0][0] - 1][q[0][1]] == -1 and trees[q[0][0] - 1][q[0][1]] > trees[q[0][0]][q[0][1]]:
-                q.append([q[0][0] - 1, q[0][1], q[0][2] + 1])
-                distance[q[0][0] - 1][q[0][1]] = q[0][2] + 1
-        if q[0][0] < N - 1:
-            if distance[q[0][0] + 1][q[0][1]] == -1 and trees[q[0][0] + 1][q[0][1]] > trees[q[0][0]][q[0][1]]:
-                q.append([q[0][0] + 1, q[0][1], q[0][2] + 1])
-                distance[q[0][0] + 1][q[0][1]] = q[0][2] + 1
-        if q[0][1] > 0:
-            if distance[q[0][0]][q[0][1] - 1] == -1 and trees[q[0][0]][q[0][1] - 1] > trees[q[0][0]][q[0][1]]:
-                q.append([q[0][0], q[0][1] - 1, q[0][2] + 1])
-                distance[q[0][0]][q[0][1] - 1] = q[0][2] + 1
-        if q[0][1] < N - 1:
-            if distance[q[0][0]][q[0][1] + 1] == -1 and trees[q[0][0]][q[0][1] + 1] > trees[q[0][0]][q[0][1]]:
-                q.append([q[0][0], q[0][1] + 1, q[0][2] + 1])
-                distance[q[0][0]][q[0][1] + 1] = q[0][2] + 1
-        if len(q) == 1:
-            maxDis = q[0][2]
-            break
-        q.popleft()
-    for i in range(N):
-        for j in range(N):
-            if distance[i][j] != -1 and lifeList[i][j] == 0:
-                lifeList[i][j] = maxDis - distance[i][j] + 1
+def dfs(board, chk, pos, day, N): #day: 현재 시간
+    y, x = pos[0], pos[1]
+    dy = [-1, 1, 0, 0]
+    dx = [0, 0, -1, 1]
+    flag = 0
+    maxDay = 0 #
+    for i in range(4):
+        ny, nx = y + dy[i], x + dx[i]
+        if 0 <= ny < N and 0 <= nx < N and board[y][x] < board[ny][nx]:
+            if chk[ny][nx] == 0: #아직 방문 안해서 길이를 구해야함
+                flag = 1
+                tmpDay = dfs(board, chk, [ny, nx], day, N)
+            else: #이미 방문해서 길이를 구한곳임
+                flag = 1
+                tmpDay = chk[ny][nx]
+            if maxDay <= day + tmpDay:
+                maxDay = day + tmpDay
+    if flag:
+        chk[y][x] = maxDay
+        return maxDay
+    else: #주변에 더 이상 방문할곳이 없음. 길이 0
+        chk[y][x] = 1
+        return 1
 
-    for i in range(N):
-        print(lifeList[i])
-    print('---------------------------------')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
