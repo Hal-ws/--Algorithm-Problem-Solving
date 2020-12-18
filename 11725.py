d1de82bea91c3e0555a14a03c46c28a23582f9e8
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2020-12-18 17:05:52 +0900

    Update 11725.py

--- 11725.py
+++ 11725.py
@@ -19,9 +19,10 @@ def main():
         l = len(connect[node]) # node와 연결된값을 고름
         for i in range(l):
             next = connect[node][i] #다음에 방문할 node
-            if parent[node] != next and visit[next] == 0: #지금 node의 부모 노드로는 안감
+            if visit[next] == 0: #지금 node의 부모 노드로는 안감
                 q.append(next)
                 parent[next] = node
+                visit[next] = 1
         q.popleft()
     for i in range(2, N + 1):
         print(parent[i])
