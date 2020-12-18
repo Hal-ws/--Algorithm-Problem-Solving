3eb6cb5cf7a8fcfde3d18b606f0108453a3516a8
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2020-12-18 17:02:16 +0900

    Create 11725.py

---
+++ 11725.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+
+
+def main():
+    N = int(sys.stdin.readline())
+    connect = [[] for i in range(N + 1)]
+    visit = [0] * (N + 1)
+    parent = [0] * (N + 1)
+    for i in range(N - 1):
+        a, b = map(int, sys.stdin.readline().split())
+        connect[a].append(b)
+        connect[b].append(a)
+    q = deque()
+    q.append(1) #q에 1 추가
+    visit[1] = 1
+    while len(q) > 0:
+        node = q[0]
+        l = len(connect[node]) # node와 연결된값을 고름
+        for i in range(l):
+            next = connect[node][i] #다음에 방문할 node
+            if parent[node] != next and visit[next] == 0: #지금 node의 부모 노드로는 안감
+                q.append(next)
+                parent[next] = node
+        q.popleft()
+    for i in range(2, N + 1):
+        print(parent[i])
+
+
+if __name__ == '__main__':
+    main()
