260cfc5fd2c7c5966a6ccc29945819078e765681
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2020-12-24 13:53:50 +0900

    Update 1113.py

--- 1113.py
+++ 1113.py
@@ -1,25 +1,68 @@
 import sys
+from collections import deque
 
 
 def main():
+    global N, M
     N, M = map(int, sys.stdin.readline().split())
-    pool = [0] * N
+    minH, maxH = 10, 0
+    pool = [[[0 for j in range(M)] for i in range(N)] for k in range(10)]
     for i in range(N):
-        temp = list(sys.stdin.readline()[:M])
+        tmp = list(map(int, sys.stdin.readline()[:M]))
         for j in range(M):
-            temp[j] = int(temp[j])
-        pool[i] = temp
-    print(pool)
+            if tmp[j] <= minH:
+                minH = tmp[j]
+            if tmp[j] >= maxH:
+                maxH = tmp[j]
+            for k in range(tmp[j] + 1):
+                pool[k][i][j] = 1 #물 채워넣음
+    answer = 0
+    for height in range(minH + 1, maxH + 1):
+        answer += getArea(pool[height])
+    print(answer)
 
 
-def horizontal(i, j, pool):
-    water = 0
-    return water
+def getArea(layer):
+    global N, M
+    q = deque()
+    visited = [[0 for j in range(M)] for i in range(N)]
+    dy, dx = [-1, 1, 0, 0], [0, 0, -1, 1]
+    cnt = 0
+    for i in range(N):
+        for j in range(M):
+            if i == 0 or j == 0 or i == N - 1 or j == M - 1:
+                if layer[i][j] == 0:# 바깥쪽이랑 무조건 연결됨
+                    q.append([i, j])
+                    layer[i][j] = -1
+                    visited[i][j] = 1
+    while len(q) > 0: #layer에서 바깥쪽 연결된 곳에 표시
+        curY, curX = q[0][0], q[0][1]
+        for i in range(4):
+            nY, nX = curY + dy[i], curX + dx[i]
+            if 0 <= nY < N and 0 <= nX < M:
+                if layer[nY][nX] == 0 and visited[nY][nX] == 0:
+                    layer[nY][nX] = -1 #밖으로 빠져나감
+                    q.append([nY, nX])
+                    visited[nY][nX] = 1
+        q.popleft()
+    q = deque()
+    for i in range(N):
+        for j in range(M):
+            if layer[i][j] == 1:
+                q.append([i, j])
+                visited[i][j] = 1
+    while len(q) > 0:
+        curY, curX = q[0][0], q[0][1]
+        for i in range(4):
+            nY, nX = curY + dy[i], curX + dx[i]
+            if 0 <= nY < N and 0 <= nX < M:
+                if layer[nY][nX] == 0 and visited[nY][nX] == 0:
+                    cnt += 1
+                    visited[nY][nX] = 1
+                    q.append([nY, nX])
+        q.popleft()
+    return cnt
 
-
-def vertical(i, j, pool):
-    water = 0
-    return water
 
 if __name__ == "__main__":
     main()
