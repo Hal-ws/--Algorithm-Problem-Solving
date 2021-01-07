9c3cecd2ec4a349d18329f85158e580fba953179
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-01-07 23:37:52 +0900

    Update 16973.py

--- 16973.py
+++ 16973.py
@@ -7,6 +7,7 @@ def main():
     board = []
     N, M = map(int, sys.stdin.readline().split())
     visit = [[0 for j in range(M)] for i in range(N)]
+    emptyChk = [[1 for j in range(M)] for i in range(N)]
     dy, dx = [-1, 1, 0, 0], [0, 0, -1, 1]
     q = deque()
     ans = -1
@@ -14,6 +15,17 @@ def main():
         board.append(list(map(int, sys.stdin.readline().split())))
     H, W, Sr, Sc, Fr, Fc = map(int, sys.stdin.readline().split())
     Sr, Sc, Fr, Fc = Sr - 1, Sc - 1, Fr - 1, Fc - 1
+    for i in range(N):
+        for j in range(M):
+            if N - H < i:
+                emptyChk[i][j] = 0
+            if M - W < j:
+                emptyChk[i][j] = 0
+            if board[i][j] == 1: # 벽이 있으면
+                for k in range(i - H + 1, i + 1):
+                    for l in range(j - W + 1, j + 1):
+                        if 0 <= k < N and 0 <= l < M:
+                            emptyChk[k][l] = 0
     visit[Sr][Sc] = 1
     q.append([Sr, Sc, 0])
     while len(q) > 0:
@@ -25,12 +37,7 @@ def main():
             ny, nx = y + dy[i], x + dx[i]
             if 0 <= ny < N and 0 <= nx < M:
                 if 0 <= ny + H - 1 < N and 0 <= nx + W - 1 < M and visit[ny][nx] == 0:
-                    flag = 1
-                    for j in range(H):
-                        if board[ny + j][nx:nx + W] != [0] * W:
-                            flag = 0
-                            break
-                    if flag:
+                    if emptyChk[ny][nx]:
                         q.append([ny, nx, cnt + 1])
                         visit[ny][nx] = 1
         q.popleft()
