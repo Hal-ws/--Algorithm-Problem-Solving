95a9d68e6c211d54c78868c760bbe579215e383d
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-02-01 01:33:20 +0900

    Update 16637.py

--- 16637.py
+++ 16637.py
@@ -14,13 +14,12 @@ def main():
     if N == 1:
         print(nList[0])
         return
-    print(nList)
     for i in range(N):
         if i % 2 == 0:
             nList[i] = int(nList[i])
     for i in range(2, N // 2 + 2, 2):
         getresult(i)
-
+    print(ans)
 
 
 def getresult(cnt): # 괄호의 개수
@@ -28,25 +27,60 @@ def getresult(cnt): # 괄호의 개수
     cases = list(combinations(iList, cnt))
     for case in cases:
         pList = [0] * N
-        print('case: %s' %list(case))
         for i in range(len(case)):
             if i % 2 == 0: #짝수일때, 괄호 start
                 pList[case[i]] = 's'
             else:
                 pList[case[i]] = 'e'
-        print(pList)
-        calculation(pList)
+        i = 0
+        flag = 1
+        while i < N:
+            if pList[i] == 's': # 괄호 시작
+                sIdx = i
+                for j in range(sIdx + 1, N):
+                    if pList[j] == 'e':
+                        eIdx = j
+                        break
+                if eIdx - sIdx > 2:
+                    flag = 0
+                    break
+                if sIdx == 0: #시작하자마자 괄호에 있는경우
+                    val = calculation(sIdx, eIdx)
+                else:
+                    if nList[sIdx - 1] == '+':
+                        val += calculation(sIdx, eIdx)
+                    if nList[sIdx - 1] == '-':
+                        val -= calculation(sIdx, eIdx)
+                    if nList[sIdx - 1] == '*':
+                        val *= calculation(sIdx, eIdx)
+                i = eIdx + 2
+            else:
+                if i == 0:
+                    val = nList[i]
+                else:
+                    if nList[i - 1] == '+':
+                        val += nList[i]
+                    if nList[i - 1] == '-':
+                        val -= nList[i]
+                    if nList[i - 1] == '*':
+                        val *= nList[i]
+                i += 2
+        if val >= ans and flag:
+            ans = val
 
 
-def calculation(pList):
+def calculation(sIdx, eIdx):
     global N, nList, ans
-    i = 0
-    val = 0
-    while i < N:
-        
-        i += 2
-
-
+    val = nList[sIdx]
+    for i in range(sIdx + 2, eIdx + 1, 2):
+        t = nList[i]
+        if nList[i - 1] == '+':
+            val += t
+        if nList[i - 1] == '-':
+            val -= t
+        if nList[i - 1] == '*':
+            val *= t
+    return val
 
 
 if __name__ == '__main__':
