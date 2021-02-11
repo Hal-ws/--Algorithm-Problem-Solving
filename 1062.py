5c21eada6e8f808876daff5954c77acae4af3933
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-02-11 18:01:56 +0900

    Update 1062.py

--- 1062.py
+++ 1062.py
@@ -3,11 +3,11 @@ import sys
 
 
 def main():
-    global masking, N, M
+    global table, total, N
     N, M = map(int, sys.stdin.readline().split())
     table = []
     total = [0] * 26
-    essential = [-1] * 26
+    essential = [1] * 26
     for i in range(N):
         word = sys.stdin.readline()
         tmp = [0] * 26
@@ -15,26 +15,46 @@ def main():
             tmp[ord(word[j]) - 97] = 1
             total[ord(word[j]) - 97] = 1
         table.append(tmp)
+    for j in range(26):
+        for i in range(N):
+            if table[i][j] == 0:
+                essential[j] = 0
+                break
+    if M < sum(essential):
+        print(0)
+        return
+    if M >= sum(total):
+        print(N)
+        return
     for i in range(N):
-        print(table[i])
-    print(total)
+        for j in range(26):
+            table[i][j] -= essential[j]
+            if i == 0:
+                total[j] -= essential[j]
+    print(getans(M - sum(essential)))
 
 
-def getans(case):
-    global masking, N, M
-    std = [0] * 26
-    for num in case:
-        std[num] = 1
-    cnt = 0
-    for i in range(N):
-        flag = 1
-        for j in range  (26):
-            if masking[i][j] == 1: #j번째 알파벳이 필요함
-                if std[j] == 0:
-                    flag = 0
-        if flag:
-            cnt += 1
-    return cnt
+def getans(tWords):
+    global table, total, N
+    tmp = []
+    for i in range(26):
+        if total[i]:
+            tmp.append(i)
+    cases = list(combinations(tmp, tWords))
+    ans = 0
+    for case in cases:
+        std = [0] * 26
+        tmp = N
+        for idx in case:
+            std[idx] = 1
+        for word in table:
+            for i in range(26):
+                if word[i] == 1 and std[i] == 0:
+                    tmp -= 1
+                    break
+        if ans < tmp:
+            ans = tmp
+    return ans
 
 
 if __name__ == "__main__":
