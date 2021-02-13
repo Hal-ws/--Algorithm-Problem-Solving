e0068b5c214df77ffe97da43fc95e466abd6b2d8
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-02-13 14:33:09 +0900

    Update 16235.py

--- 16235.py
+++ 16235.py
@@ -2,66 +2,44 @@ import sys
 from collections import deque
 
 
-def main():
-    N, M, K = map(int, sys.stdin.readline().split())
-    A, trees, deadtrees = [], deque(), deque()
-    field = [[5 for i in range(N)] for j in range(N)]
-    dx = [-1, -1, -1, 0, 0, 1, 1, 1]
-    dy = [-1, 0, 1, -1, 1, -1, 0, 1]
-    #A: 매 겨울마다 밭에 더해지는 양분 field: 현재 밭의 양분 상태. trees: 지금 밭에 있는 나무, deadtrees: 죽은 나무
-    for i in range(N):
-        A.append(list(map(int, sys.stdin.readline().split())))
-        field.append(A[i])
-    for i in range(M):
-        temp = list(map(int, sys.stdin.readline().split()))
-        trees.append([N - temp[1], temp[0] - 1, temp[2]])
-    lt = len(trees)
-    for i in range(K):
-        lt = spring(trees, deadtrees, field, lt)
-        summer(deadtrees, field)
-        lt = fall(trees, dx, dy, lt, N)
-        winter(A, field, N)
-    print(lt)
-
-
-def spring(trees, deadtrees, field, lt):
-    for i in range(lt):
-        if trees[0][2] > field[trees[0][0]][trees[0][1]]: #트리가 죽음
-            deadtrees.append(trees.popleft())
-            lt -= 1
-        else:
-            field[trees[0][0]][trees[0][1]] -= trees[0][2]
-            trees[0][2] += 1
-            trees.append(trees.popleft())
-    return lt
-
-
-def summer(deadtrees, field):
-    ld = len(deadtrees)
-    for i in range(ld):
-        temp = deadtrees.popleft()
-        field[temp[0]][temp[1]] += (temp[2] // 2)
-
-
-def fall(trees, dx, dy, lt, N):
-    adding = 0
-    for i in range(lt):
-        if trees[i + adding][2] % 5 == 0:
-            for j in range(8):
-                x = trees[i + adding][1] + dx[j]
-                y = trees[i + adding][0] + dy[j]
-                if 0 <= x < N and 0 <= y < N:
-                    trees.appendleft([y, x, 1])
-                    adding += 1
-                    lt += 1
-    return lt
-
-
-def winter(A, field, N):
+N, M, K = map(int, sys.stdin.readline().split())
+dy, dx = [-1, -1, -1, 0, 0, 1, 1, 1], [-1, 0, 1, -1, 1, -1, 0, 1]
+ground = [[5 for j in range(N)] for i in range(N)]
+trees = [[deque() for j in range(N)] for i in range(N)]
+add, deadTree = [], []
+for i in range(N):
+    add.append(list(map(int, sys.stdin.readline().split())))
+for i in range(M):
+    x, y, z = map(int, sys.stdin.readline().split())
+    trees[x - 1][y - 1].append(z)
+for year in range(K):
+    newTree = [[deque() for j in range(N)] for i in range(N)]
     for i in range(N):
         for j in range(N):
-            field[i][j] += A[i][j]
-
-
-if __name__ == "__main__":
-    main()
+            rotten = 0
+            l = len(trees[i][j])
+            spread = []
+            for t in range(l):
+                age = trees[i][j].popleft()
+                if age <= ground[i][j]:
+                    ground[i][j] -= age
+                    newTree[i][j].append(age + 1)
+                    if (age + 1) % 5 == 0:
+                        spread.append(age + 1)
+                else:
+                    rotten += (age // 2)
+            ground[i][j] += rotten
+            for t in spread:
+                for k in range(8):
+                    ny, nx = i + dy[k], j + dx[k]
+                    if 0 <= ny < N and 0 <= nx < N:
+                        newTree[ny][nx].appendleft(1)
+    trees = newTree
+    for i in range(N):
+        for j in range(N):
+            ground[i][j] += add[i][j]
+ans = 0
+for i in range(N):
+    for j in range(N):
+        ans += len(trees[i][j])
+print(ans)
