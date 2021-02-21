bf016819833fbe22c5e4250ac18ac3c8ca0bbf0d
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-02-21 20:43:24 +0900

    Update 14238.py

--- 14238.py
+++ 14238.py
@@ -1,47 +1,53 @@
 def main():
+    global ansl
     a = input()
-    l = len(a)
-    cnt = [0, 0, 0] # A, B, C의 갯수
-    for p in a:
-        cnt[ord(p) - 65] += 1
-    dp = [[[[] for i in range(l + 1)] for j in range(l + 1)] for k in range(l + 1)]
-    for i in range(cnt[0] + 1):
-        for j in range(cnt[1] + 1):
-            for k in range(cnt[2] + 1):
-                if i == 1 and j == 0 and k == 0:
-                    dp[i][j][k].append('A')
-                elif i == 0 and j == 1 and k == 0:
-                    dp[i][j][k].append('B')
-                elif i == 0 and j == 0 and k == 1:
-                    dp[i][j][k].append('C')
-                elif i > 0 or j > 0 or k > 0:
-                    l = i + j + k
-                    if i >= 1:
-                        tmp = dp[i - 1][j][k] # A를 추가해줄것
-                        if tmp != []: #불가능한 경우가 아니면
-                            for word in tmp:
-                                dp[i][j][k].append(word + 'A')
-                    if j >= 1:
-                        tmp = dp[i][j - 1][k] # B를 추가
-                        if tmp != []:
-                            for word in tmp:
-                                if word[l - 2] != 'B':
-                                    dp[i][j][k].append(word + 'B')
-                    if k >= 1:
-                        tmp = dp[i][j][k - 1] # C를 추가
-                        if tmp != []:
-                            for word in tmp:
-                                if l == 2:
-                                    if word[l - 2] != 'C':
-                                        dp[i][j][k].append(word + 'C')
-                                if l > 2:
-                                    if word[l - 2] != 'C' and word[l - 3] != 'C':
-                                        dp[i][j][k].append(word + 'C')
-    ans = dp[cnt[0]][cnt[1]][cnt[2]]
-    if ans == []:
-        print(-1)
-    else:
-        print(ans[0])
+    ansl = len(a)
+    ans = -1
+    cCnt = [0, 0, 0] # a, b, c 기록
+    for c in a:
+        cCnt[ord(c) - 65] += 1
+    for i in range(3):
+        if cCnt[i] > 0:
+            tmp = getans(i, [cCnt[0], cCnt[1], cCnt[2]])
+            if tmp != -1:
+                ans = tmp
+                break
+    print(ans)
+
+def getans(sIdx, cntList):
+    global ansl
+    word = chr(sIdx + 65)
+    cntList[sIdx] -= 1
+    dis = [51, 51, 51] # A로부터 거리, B로부터 거리, C로부터 거리
+    dis[sIdx] = 0 # 붙어있음
+    while 1:
+        if len(word) == ansl:
+            return word
+        left = ansl - len(word) - 1 # 지금 시행에서 붙이고 남은 길이
+        maxB = left // 2 #B를 붙일 수 있는 최대 갯수
+        maxC = left // 3 #C를 붙일 수 있는 최대 갯수
+        if left % 2 != 0:
+            maxB += 1
+        if left % 3 != 0:
+            maxC += 1
+        # C붙임
+        if cntList[2] > 0 and cntList[1] <= maxB and dis[2] >= 2:
+            word += 'C'
+            cntList[2] -= 1
+            dis[0], dis[1], dis[2] = dis[0] + 1, dis[1] + 1, 0
+            continue
+        if cntList[1] > 0 and cntList[2] <= maxC and dis[1] >= 1:
+            word += 'B'
+            cntList[1] -= 1
+            dis[0], dis[1], dis[2] = dis[0] + 1, 0, dis[2] + 1
+            continue
+        if cntList[0] > 0:
+            word += 'A'
+            cntList[0] -= 1
+            dis[0], dis[1], dis[2] = 0, dis[1] + 1, dis[2] + 1
+            continue
+        break
+    return -1
 
 
 if __name__ == '__main__':
