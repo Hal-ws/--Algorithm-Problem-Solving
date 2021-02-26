206a45e7ba12172e7953e298e39a5af0e98e2648
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-02-26 17:29:35 +0900

    Update 4574.py

--- 4574.py
+++ 4574.py
@@ -3,8 +3,9 @@ from itertools import combinations
 
 
 def main():
-    global board, garo, sero, box, flag, blocks
+    global board, garo, sero, box, flag, blocks, dy, dx
     t = 1
+    dy, dx = [-1, -1, -1, 0, 0, 0, 1, 1, 1], [-1, 0, 1, -1, 0, 1, -1, 0, 1]
     while 1:
         N = int(sys.stdin.readline())
         if N == 0:
@@ -54,23 +55,96 @@ def backtraking(y, x, emptyCnt, possIdx):
         if board[y][j] == 0: #
             if j < 8 and board[y][j + 1] == 0: # 가로로 넣어봄
                 for k in range(45):
-                    if chkPossible(blocks[k], 0): # 그대로 가능
-                        possIdx[k] = 0
-                        backtraking(y, x + 2, emptyCnt - 2, possIdx)
-                        possIdx[k] = 1
-                    if chkPossible(blocks[k], 1): # 뒤집은게 가능
-                        possIdx[k] = 0
-                        backtraking(y, x + 2, emptyCnt - 2, possIdx)
-                        possIdx[k] = 1
-                
-
+                    if possIdx[k]: # 쓸수 있는 block일때
+                        if chkPossible(y, j, blocks[k], 0, 0): # 그대로 가능
+                            possIdx[k] = 0
+                            board[y][j], board[y][j + 1] = blocks[k][0], blocks[k][1]
+                            backtraking(y, j + 2, emptyCnt - 2, possIdx)
+                            board[y][j], board[y][j + 1] = 0, 0
+                            possIdx[k] = 1
+                        if chkPossible(y, j, blocks[k], 0, 1): # 뒤집은게 가능
+                            possIdx[k] = 0
+                            board[y][j], board[y][j + 1] = blocks[k][1], blocks[k][0]
+                            backtraking(y, j + 2, emptyCnt - 2, possIdx)
+                            board[y][j], board[y][j + 1] = 0, 0
+                            possIdx[k] = 1
+            if y < 8 and board[y + 1][j] == 0: #세로로 넣어봄
+                for k in range(45):
+                    if possIdx[k]:
+                        if chkPossible(y, j, blocks[k], 1, 0):
+                            possIdx[k] = 0
+                            board[y][j], board[y + 1][j] = blocks[k][0], blocks[k][1]
+                            backtraking(y + 2, j, emptyCnt - 2, possIdx)
+                            board[y][j], board[y + 1][j] = 0, 0
+                            possIdx[k] = 1
+                        if chkPossible(y, j, blocks[k], 1, 1):
+                            possIdx[k] = 0
+                            board[y][j], board[y + 1][j] = blocks[k][1], blocks[k][0]
+                            backtraking(y + 2, j, emptyCnt - 2, possIdx)
+                            board[y][j], board[y + 1][j] = 0, 0
+                            possIdx[k] = 1
     for i in range(y + 1, 9):
         for j in range(9):
-            return
+            if j < 8 and board[i][j + 1] == 0: # 가로로 넣어봄
+                for k in range(45):
+                    if possIdx[k]: # 쓸수 있는 block일때
+                        if chkPossible(i, j, blocks[k], 0, 0): # 그대로 가능
+                            possIdx[k] = 0
+                            board[i][j], board[i][j + 1] = blocks[k][0], blocks[k][1]
+                            backtraking(i, j + 2, emptyCnt - 2, possIdx)
+                            board[i][j], board[i][j + 1] = 0, 0
+                            possIdx[k] = 1
+                        if chkPossible(i, j, blocks[k], 0, 1): # 뒤집은게 가능
+                            possIdx[k] = 0
+                            board[i][j], board[i][j + 1] = blocks[k][1], blocks[k][0]
+                            backtraking(i, j + 2, emptyCnt - 2, possIdx)
+                            board[i][j], board[i][j + 1] = 0, 0
+                            possIdx[k] = 1
+            if y < 8 and board[y + 1][j] == 0: #세로로 넣어봄
+                for k in range(45):
+                    if possIdx[k]:
+                        if chkPossible(i, j, blocks[k], 1, 0):
+                            possIdx[k] = 0
+                            board[i][j], board[i + 1][j] = blocks[k][0], blocks[k][1]
+                            backtraking(i + 2, j, emptyCnt - 2, possIdx)
+                            blocks[i][j], blocks[i + 1][j] = 0, 0
+                            possIdx[k] = 1
+                        if chkPossible(i, j, blocks[k], 1, 1):
+                            possIdx[k] = 0
+                            board[i][j], board[i + 1][j] = blocks[k][1], blocks[k][0]
+                            backtraking(i + 2, j, emptyCnt - 2, possIdx)
+                            board[i][j], board[i + 1][j] = 0, 0
+                            possIdx[k] = 1
 
 
-def chkPossible(domino, rFlag):
-    global board
+def chkPossible(y, x, domino, vFlag, rFlag): #도미노, 가로세로 구분, 뒤집는것
+    global board, dy, dx
+    if rFlag:
+        domino = domino[::-1]
+    c1 = [1 + (y // 3) * 3, 1 + (x // 3) * 3]
+    if vFlag:
+        for j in range(9):
+            if board[y][j] == domino[0]:
+                return 0
+            if board[y + 1][j] == domino[1]:
+                return 0
+        for i in range(9):
+            if board[i][x] == domino[0] or board[i][x] == domino[1]:
+                return 0
+        c2 = [1 + ((y + 1) // 3) * 3, 1 + (x // 3) * 3]
+    else:
+        for j in range(9):
+            if board[y][j] == domino[0] or board[y][j] == domino[1]:
+                return 0
+        for i in range(9):
+            if board[i][x] == domino[0]:
+                return 0
+            if board[i][x + 1] == domino[1]:
+                return 0
+        c2 = [1 + (y // 3) * 3, 1 + ((x + 1) // 3) * 3]
+    return 1
+
+
     return
 
 
