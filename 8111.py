6c9c26a71b6a749fbb052577a51b1a3e6356c478
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-03-02 10:12:42 +0900

    Update 8111.py

--- 8111.py
+++ 8111.py
@@ -1,44 +1,29 @@
 import sys
-from collections import deque
+from _collections import deque
 
 
 def main():
     T = int(sys.stdin.readline())
-    for i in range(T):
+    for _ in range(T):
         N = int(sys.stdin.readline())
+        visit = [0] * (N + 1)
         q = deque()
-        ans = None
-        for j in range(1, 10):
-            if (N * j) % 10 == 1 or (N * j) % 10 == 0:
-                q.append([str(j), 1, len(str(N))])
+        q.append(1)
+        visit[1] = 1
+        ans = 'BRAK'
         while len(q) > 0:
-            mVal, l, lenN = q[0][0], q[0][1], q[0][2] # 약수, 정해진 자릿수의 길이, 현재 값의 길이
-            if lenN > 100:
-                q.popleft()
-                continue
-            num = N * int(mVal)
-            if l == lenN:
-                ans = num
-            sNum = str(num)
-            ansFlag = 1
-            for j in range(lenN - l - 1, -1, -1):
-                if sNum[j] != '1' and sNum[j] != '0':
-                    ansFlag = 0
-            if ansFlag: #답이 될 때
-                ans = sNum
+            val = q.popleft()
+            if val % N == 0:
+                ans = val
                 break
-            for j in range(1, 10): #
-                tmp = num + (N * j * pow(10, l))
-                tmpS = str(tmp)
-                lt = len(tmpS)
-                if tmpS[lt - 1 - l] == '0' or tmpS[lt - 1 - l] == '1':
-                    q.append([tmp // N, l + 1, lt])
-            q.popleft()
-        if ans == None:
-            print('BRAK')
-        else:
-            print(ans)
+            if visit[(val * 10) % N] == 0:
+                q.append(val * 10)
+                visit[(val * 10) % N] = 1
+            if visit[(val * 10 + 1) % N] == 0:
+                q.append(val * 10 + 1)
+                visit[(val * 10 + 1) % N] = 1
+        print(ans)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
