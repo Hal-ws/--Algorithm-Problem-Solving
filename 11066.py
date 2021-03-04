93e843febf99eebb8ca38fee34f323054a4f7a15
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-03-04 21:04:31 +0900

    Update 11066.py

--- 11066.py
+++ 11066.py
@@ -7,24 +7,25 @@ def main():
     for t in range(T):
         K = int(sys.stdin.readline())
         files = list(map(int, sys.stdin.readline().split()))
-        dp = [[0 for j in range(K)] for i in range(K)]
+        dp = [[[0, 0] for j in range(K)] for i in range(K)] # 합, 누적 비용
         for l in range(1, K + 1): # 길이가 2개부터 K까지
             for i in range(K - l + 1):
                 j = i + l - 1 # i부터 j까지 합의 최소값 구함
                 if i == j:
-                    dp[i][j] = files[i]
+                    dp[i][j][0], dp[i][j][1] = files[i], 0
                 elif j == i + 1:
-                    dp[i][j] = files[i] + files[j]
+                    dp[i][j][0], dp[i][j][1] = files[i] + files[j], files[i] + files[j]
                 else:
-                    minV = inf
+                    minSum = inf
+                    minCost = inf
                     for d in range(i, j): #i 부터 j 사이에 자름
-                        tmp = (dp[i][d] + dp[d + 1][j]) * 2
-                        if tmp < minV:
-                            minV = tmp
-                    dp[i][j] = minV
-        for i in range(K):
-            print(dp[i])
-        print(dp[0][K - 1])
+                        fileSum = dp[i][d][0] + dp[d + 1][j][0]
+                        costSum = dp[i][d][1] + dp[d + 1][j][1] + fileSum
+                        if costSum < minCost:
+                            minSum = fileSum
+                            minCost = costSum
+                    dp[i][j] = [minSum, minCost]
+        print(dp[0][K - 1][1])
 
 
 if __name__ == '__main__':
