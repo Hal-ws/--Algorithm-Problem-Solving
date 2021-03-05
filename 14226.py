320e8dce012ba07019caed498520a0c9a8e66a59
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-03-05 23:57:27 +0900

    Update 14226.py

--- 14226.py
+++ 14226.py
@@ -1,34 +1,29 @@
+import sys
 from collections import deque
 
 
 def main():
-    S = int(input())
-    chk = [[-1 for i in range(2001)] for j in range(2001)] #도착시간 걸어둠
-    chk[1][0] = 0
-    chk[2][1] = 2
+    N = int(sys.stdin.readline())
+    visit = [[0 for j in range(2001)] for i in range(2001)]
+    visit[1][0] = 1
     q = deque()
-    q.append([1, 0, 0]) # 개수, 클립보드 저장된 양, 시간
-    q.append([2, 1, 2])
+    q.append([1, 0, 0]) # 이모티콘 길이, 클립보드에 저장된 길이
     while len(q) > 0:
-        if q[0][0] * 2 <= 2000: # 2배 곱함
-            if chk[q[0][0] * 2][q[0][0]] == - 1 or chk[q[0][0] * 2][q[0][0]] > q[0][2] + 2:
-                chk[q[0][0] * 2][q[0][0]] = q[0][2] + 2
-                q.append([q[0][0] * 2, q[0][0], q[0][2] + 2])
-        if q[0][0] + q[0][1] <= 2000: # 지금 클립보드에 저장된값 더함
-            if chk[q[0][0] + q[0][1]][q[0][1]] == -1 or chk[q[0][0] + q[0][1]][q[0][1]] > q[0][2] + 1:
-                chk[q[0][0] + q[0][1]][q[0][1]] = q[0][2] + 1
-                q.append([q[0][0] + q[0][1], q[0][1], q[0][2] + 1])
-        if q[0][0] - 1 >= 0:
-            if chk[q[0][0] - 1][q[0][1]] == -1 or chk[q[0][0] - 1][q[0][1]] > q[0][2] + 1:
-                chk[q[0][0] - 1][q[0][1]] = q[0][2] + 1
-                q.append([q[0][0] - 1, q[0][1], q[0][2] + 1])
-        q.popleft()
-    ans = 9999999999999999999
-    for i in range(2000):
-        if chk[S][i] != -1 and chk[S][i] <= ans:
-            ans = chk[S][i]
-    print(ans)
+        tmp = q.popleft()
+        l, clip, t = tmp[0], tmp[1], tmp[2]
+        if l == N:
+            print(t)
+            break
+        if l + clip <= 2000 and visit[l + clip][clip] == 0:
+            visit[l + clip][clip] = 1
+            q.append([l + clip, clip, t + 1])
+        if visit[l][l] == 0:
+            visit[l][l] = 1
+            q.append([l, l, t + 1])
+        if l - 1 >= 0 and visit[l - 1][clip] == 0:
+            visit[l - 1][clip] = 1
+            q.append([l - 1, clip, t + 1])
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
