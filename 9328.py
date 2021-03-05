212bf7a38e514296c5118d5bc2477a3a361f191e
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-03-05 14:25:53 +0900

    Update 9328.py

--- 9328.py
+++ 9328.py
@@ -3,70 +3,58 @@ from collections import deque
 
 
 def main():
-    global board, h, w, ans, dy, dx
+    global h, w, board, dy, dx, ans
     T = int(sys.stdin.readline())
     dy, dx = [-1, 1, 0, 0], [0, 0, -1, 1]
     for _ in range(T):
-        h, w = map(int, sys.stdin.readline().split())
-        ans = 0
         board = []
+        h, w = map(int, sys.stdin.readline().split())
+        board.append(['.'] * (w + 2))
         for i in range(h):
-            board.append(list(sys.stdin.readline()[:w]))
+            board.append(['.'] + list(sys.stdin.readline()[:w]) + ['.'])
+        board.append(['.'] * (w + 2))
+        h, w = h + 2, w + 2
         iniKey = sys.stdin.readline()
-        iniKey = iniKey[:len(iniKey) - 1]
-        if iniKey != '0':
-            key = [0] * 26
-            for k in iniKey:
-                key[ord(k) - 97] = 1
-        for i in range(h):
-            for j in range(w):
-                if i == 0 or j == 0 or i == h - 1 or j == w - 1:
-                    if board[i][j] == '.':
-                        getChk = [[0 for j in range(w)] for i in range(h)]
-                        bfs([i, j], key, 0, getChk)
-                    elif board[i][j] == '$':
-                        getChk = [[0 for j in range(w)] for i in range(h)]
-                        getChk[i][j] = 1
-                        bfs([i, j], key, 1, getChk)
-                    elif board[i][j] != '*':
-                        c = board[i][j]
-                        getChk = [[0 for j in range(w)] for i in range(h)]
-                        if 65 <= ord(c) <= 90: # 대문자일때
-                            if key[ord(c) - 65]: # 열쇠가 있을때
-                                bfs([i, j], key, 0, getChk)
-                        else:
-                            key[ord(c) - 97] = 1
-                            bfs([i, j], key, 0, getChk)
-                            key[ord(c) - 97] = 0
+        key = [0] * 26
+        ans = 0
+        if iniKey[0] != '0':
+            for i in range(len(iniKey) - 1):
+                key[ord(iniKey[i]) - 97] = 1
+        bfs([0, 0], 0, key)
         print(ans)
 
 
-def bfs(start, key, cnt, getChk):
-    global board, h, w, ans, dy, dx
+def bfs(start, cnt, key): #위치, 훔친 문서 수, 갖고 있는 key
+    global h, w, board, dy, dx, ans
+    if ans < cnt:
+        ans = cnt
     visit = [[0 for j in range(w)] for i in range(h)]
     visit[start[0]][start[1]] = 1
     q = deque()
     q.append(start)
-    newKey = [key[i] for i in range(26)]
     while len(q) > 0:
         y, x = q[0][0], q[0][1]
         for i in range(4):
             ny, nx = y + dy[i], x + dx[i]
-            if 0 <= ny < h and 0 <= nx < w and visit[ny][nx] == 0 and board[ny][nx] != '*':
-                if board[ny][nx] != '.':
-                    if board[ny][nx] == '$':
-                        visit[ny][nx] = 1
-                        getChk[ny][nx] = 1
-                        cnt += 1
-                    else:
-                        if 65 <= ord(board[ny][nx]) <= 90:
-                            if key[ord(board[ny][nx]) - 65]: #키를 갖고 있을때
-                else:
+            if 0 <= ny < h and 0 <= nx < w and board[ny][nx] != '*' and visit[ny][nx] == 0:
+                bNum = ord(board[ny][nx])
+                if bNum == 46:
                     visit[ny][nx] = 1
-                    q.append([ny, nx, cnt, newKey])
-
-
-    return 0
+                    q.append([ny, nx])
+                if 65 <= bNum <= 90 and key[bNum - 65]: # 문을 만났을때
+                    visit[ny][nx] = 1
+                    q.append([ny, nx])
+                if 97 <= bNum <= 122: # 열쇠
+                    key[bNum - 97] = 1
+                    board[ny][nx] = '.'
+                    bfs([ny, nx], cnt, key)
+                    key[bNum - 97] = 0
+                    board[ny][nx] = chr(bNum)
+                if bNum == 36: # 문서 get
+                    board[ny][nx] = '.'
+                    bfs([ny, nx], cnt + 1, key)
+                    board[ny][nx] = chr(bNum)
+        q.popleft()
 
 
 if __name__ == '__main__':
