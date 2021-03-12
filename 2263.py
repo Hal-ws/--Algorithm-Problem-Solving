e1e7df7e5218104a7851f660d88fe11ee0e208de
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-03-12 16:46:20 +0900

    Update 2263.py

--- 2263.py
+++ 2263.py
@@ -3,19 +3,18 @@ sys.setrecursionlimit(pow(10, 8))
 
 
 def main():
-    global inOrder, postOrder, inIdx, postIdx
+    global inOrder, postOrder, inIdx
     N = int(sys.stdin.readline())
-    inIdx, postIdx = {}, {}
+    inIdx = {}
     inOrder = list(map(int, sys.stdin.readline().split()))
     postOrder = list(map(int, sys.stdin.readline().split()))
     for i in range(N):
         inIdx[inOrder[i]] = i
-        postIdx[postOrder[i]] = i
     printPre(0, N - 1, 0, N - 1)
 
 
-def printPre(sIn, eIn, sPost, ePost): # 각각 In, Post order가 시작 / 끝나는 index 표시
-    global inOrder, postOrder, inIdx, postIdx
+def printPre(sIn, eIn, sPost, ePost):
+    global inOrder, postOrder, inIdx
     if sIn > eIn:
         return
     root = postOrder[ePost]
@@ -24,12 +23,8 @@ def printPre(sIn, eIn, sPost, ePost): # 각각 In, Post order가 시작 / 끝나
     l1, l2 = rIdx1 - sIn, eIn - rIdx1
     if sPost == ePost:
         return
-    nxtSIn, nxtEIn = sIn, rIdx1 - 1
-    nxtSPost, nxtEPost = sPost, sPost + l1 - 1
-    printPre(nxtSIn, nxtEIn, nxtSPost, nxtEPost)
-    nxtSIn, nxtEIn = rIdx1 + 1, eIn
-    nxtSPost, nxtEPost = ePost - l2, ePost - 1
-    printPre(nxtSIn, nxtEIn, nxtSPost, nxtEPost)
+    printPre(sIn, rIdx1 - 1, sPost, sPost + l1 - 1)
+    printPre(rIdx1 + 1, eIn, ePost - l2, ePost - 1)
 
 
 if __name__ == '__main__':
