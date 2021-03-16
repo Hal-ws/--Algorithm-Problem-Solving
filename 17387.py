f1fe2f4f626e943cbc81ec06fa08c6ec288a2a2b
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-03-17 01:56:00 +0900

    Update 17387.py

--- 17387.py
+++ 17387.py
@@ -1,55 +1,37 @@
+import sys
+
+
 def main():
-    x1, y1, x2, y2 = map(float, input().split())
-    x3, y3, x4, y4 = map(float, input().split())
+    x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
+    x3, y3, x4, y4 = map(int, sys.stdin.readline().split())
     l1 = sorted([[x1, y1], [x2, y2]])
     l2 = sorted([[x3, y3], [x4, y4]])
-    if x1 != x2 and x3 != x4:
-        m1, m2 = (y2 - y1) / (x2 - x1), (y4 - y3) / (x4 - x3)
-        if m1 == m2:
-            print(parallel(l1, l2, m1))
+    x1, y1, x2, y2 = l1[0][0], l1[0][1], l1[1][0], l1[1][1]
+    x3, y3, x4, y4 = l2[0][0], l2[0][1], l2[1][0], l2[1][1]
+    if x1 == x2 and x3 != x4:
+        if x1 < x3 or x1 > x4:
+            ans = 0
         else:
-            print(crossing(l1, l2, m1, m2))
-    elif x1 == x2 and x3 != x4:
-        m2 = (y4 - y3) / (x4 - x3)
-        y = m2(x1 - x3) + m2 * (-1) * l2[0][0] + l2[0][1]
-        if l1[0][1] <= y <= l1[1][1] and l2[0][1] <= y <= l2[1][1]:
-            print(1)
-        else:
-            print(0)
+            if y1 <= (y4 - y3) / (x4 - x3) * (x1 - x3) + y3 <= y2:
+                ans = 1
+            else:
+                ans = 0
     elif x1 != x2 and x3 == x4:
-        m1 = (y2 - y1) / (x2 - x1)
-        y = m1(x3 - x1) + m1 * (-1) * l1[0][0] + l1[0][1]
-        if l2[0][1] <= y <= l2[1][1] and l1[0][1] <= y <= l1[1][1]:
-            print(1)
+        if x3 < x1 or x2 < x3:
+            ans = 0
+    elif x1 == x2 and x3 == x4:
+        if x1 != x3:
+            ans = 0
         else:
-            print(0)
+            if y1 <= y4 <= y2 or y3 <= y2 <= y4:
+                ans = 1
+            else:
+                ans = 0
     else:
-        if l1[0][1] <= l2[0][1] <= l1[1][1] or l1[0][1] <= l2[1][1] <= l1[1][1]:
-            print(1)
-        else:
-            print(0)
+        if (y2 - y1) * (x4 - x3) == (y4 - y3) * (x2 - x1): # 기울기 같음
+            if
+    print(ans)
 
 
-def parallel(l1, l2, m1):
-    y1, y2 = m1 * (0 - l1[0][0]) + l1[0][1], m1 * (0 - l2[0][0]) + l2[0][1]
-    if y1 != y2:
-        return 0
-    else:
-        if l1[0][0] <= l2[0][0] <= l1[1][0] or l1[0][0] <= l2[1][0] <= l1[0][0]:
-            return 1
-        else:
-            return 0
-
-
-def crossing(l1, l2, m1, m2):
-    a1 = m1 * (-1) * l1[0][0] + l1[0][1]
-    a2 = m2 * (-1) * l2[0][0] + l2[0][1]
-    crossp = (a1 - a2) / (m2 - m1)
-    if l1[0][0] <= crossp <= l1[1][0]:
-        return 1
-    else:
-        return 0
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
