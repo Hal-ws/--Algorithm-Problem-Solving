2228acd7e5ec5d9564acfcdd0757ea509b04d541
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-03-19 17:17:42 +0900

    Update 3197.py

--- 3197.py
+++ 3197.py
@@ -1,68 +1,96 @@
-from collections import deque
 import sys
+from _collections import deque
 
 
 def main():
-    global R, C, board, swan, dy, dx, ans, area
+    global R, C, dy, dx, board, area, rootIdx
     R, C = map(int, sys.stdin.readline().split())
-    swan, board = [],  []
     dy, dx = [-1, 1, 0, 0], [0, 0, -1, 1]
-    day, ans = 0, -1
-    area = [[0 for j in range(C)] for i in range(R)]
+    board = []
+    swan = []
+    area = [[-1 for j in range(C)] for i in range(R)]
+    melting = [] # 이 좌표 주변에 있는 얼음덩어리들을 녹임
+    rootIdx = [] # 해당 idx의 root를 기록
     for i in range(R):
         board.append(list(sys.stdin.readline()[:C]))
-    melt = deque()
-    mltCnt = 0
+    t, cnt = 0, 0
     for i in range(R):
         for j in range(C):
+            if (board[i][j] == '.' or board[i][j] == 'L') and area[i][j] == -1:
+                bfs(i, j, cnt)
+                rootIdx.append(cnt)
+                cnt += 1
             if board[i][j] == 'L':
                 swan.append([i, j])
-            if board[i][j] == 'X': #얼음
-                for k in range(4):
-                    if (0 <= i + dy[k] < R and 0 <= j + dx[k] < C) and (board[i + dy[k]][j + dx[k]] == '.' or board[i + dy[k]][j + dx[k]] == 'L'):
-                        melt.append([i, j]) # 다음에 녹을 얼음
-                        mltCnt += 1
-                        break
-            if board[i][j] == '.':
-                area[i][j] = 8
-    while len(melt) > 0:
-        mltIce = melt.popleft()
-        area[mltIce[0]][mltIce[1]] = 3
-    bfs(swan[0], 1)
-    if ans == 0:
-        print(0)
+    s1y, s1x, s2y, s2x = swan[0][0], swan[0][1], swan[1][0], swan[1][1]
+    if area[s1y][s1x] == area[s1y][s2x]: # 시작부터 같은 곳에 있음
+        print(t)
         return
-    bfs(swan[1], 2)
+    #녹을 얼음들 리스트 만듬
     for i in range(R):
-        print(board[i])
+        for j in range(C):
+            if area[i][j] != -1: # 물일때
+                for k in range(4):
+                    ny, nx = i + dy[k], j + dx[k]
+                    if 0 <= ny < R and 0 <= nx < C and area[ny][nx] == -1: # 얼음 발견
+                        melting.append([i, j])
     for i in range(R):
         print(area[i])
+    print('rootIdx: %s' %rootIdx)
+    print('--------------------')
+    while 1:
+        print('s1 idx, s2 idx: %s, %s' %(area[s1y][s1x], area[s2y][s2x]))
+        if findroot(area[s1y][s1x]) == findroot(area[s2y][s2x]):
+            print(t)
+            break
+        nxtMelt = []
+        for water in melting: # water 주변의 ice가 녹음
+            y, x = water[0], water[1]
+            for i in range(4):
+                ny, nx = y + dy[i], x + dx[i]
+                if 0 <= ny < R and 0 <= nx < C:
+                    if area[ny][nx] == -1: # 얼음을 녹임
+                        area[ny][nx] = area[y][x]
+                        nxtMelt.append([ny, nx])
+                    elif area[y][x] != area[ny][nx]:
+                        idx1, idx2 = area[y][x], area[ny][nx]
+                        joint(idx1, idx2)
+        for i in range(R):
+            print(area[i])
+        print('rootIdx: %s' %rootIdx)
+        print('-----------------------')
+        melting = [nxtMelt[i] for i in range(len(nxtMelt))]
+        t += 1
 
 
+def joint(idx1, idx2):
+    global rootIdx
+    if rootIdx[idx1] != rootIdx[idx2]: # 서로 다른 그룹일 때
+        rootIdx[idx2] = rootIdx[idx1]
 
 
-def bfs(pos, val):
-    global R, C, board, ans, dy, dx, area
-    y, x = pos[0], pos[1]
-    visit = [[0 for j in range(C)] for i in range(R)]
+def findroot(areaI): # area idx의 root를 구함
+    global rootIdx
+    if rootIdx[areaI] == areaI:
+        return areaI
+    rootIdx[areaI] = findroot(rootIdx[areaI])
+    return rootIdx[areaI]
+
+
+def bfs(y, x, cnt):
+    global R, C, dy, dx, board, area
     q = deque()
+    area[y][x] = cnt
     q.append([y, x])
-    visit[y][x] = 1
-    area[y][x] = val # 백조가 있는 구역 표시
     while len(q) > 0:
-        y, x = q[0][0], q[0][1]
+        tmp = q.popleft()
+        y, x = tmp[0], tmp[1]
         for i in range(4):
             ny, nx = y + dy[i], x + dx[i]
-            if 0 <= ny < R and 0 <= nx < C and visit[ny][nx] == 0:
-                if board[ny][nx] == 'L': #같은 구간 안에 백조가 있을때
-                    ans = 0
-                    return
-                elif board[ny][nx] == '.':
-                    visit[ny][nx] = 1
-                    area[ny][nx] = val
-                    q.append([ny, nx])
-        q.popleft()
-                
+            if 0 <= ny < R and 0 <= nx < C and area[ny][nx] == -1 and board[ny][nx] != 'X':
+                q.append([ny, nx])
+                area[ny][nx] = cnt
+
 
 if __name__ == '__main__':
     main()
