9989cd81723946beed71a6979fb030e92c14db28
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-03-21 22:07:17 +0900

    Update 3197.py

--- 3197.py
+++ 3197.py
@@ -30,43 +30,43 @@ def main():
     for i in range(R):
         for j in range(C):
             if area[i][j] != -1: # 물일때
+                flag = 0
                 for k in range(4):
                     ny, nx = i + dy[k], j + dx[k]
                     if 0 <= ny < R and 0 <= nx < C and area[ny][nx] == -1: # 얼음 발견
-                        melting.append([i, j])
-    for i in range(R):
-        print(area[i])
-    print('rootIdx: %s' %rootIdx)
-    print('--------------------')
+                        flag = 1
+                if flag:
+                    melting.append([i, j]) # 가장자리에 있는 얼음들 위치 저장
     while 1:
-        print('s1 idx, s2 idx: %s, %s' %(area[s1y][s1x], area[s2y][s2x]))
         if findroot(area[s1y][s1x]) == findroot(area[s2y][s2x]):
             print(t)
             break
         nxtMelt = []
         for water in melting: # water 주변의 ice가 녹음
-            y, x = water[0], water[1]
+            y, x = water[0], water[1] # 현재 물의 가장자리
             for i in range(4):
-                ny, nx = y + dy[i], x + dx[i]
+                ny, nx = y + dy[i], x + dx[i] # 가장자리 주변의 좌표
                 if 0 <= ny < R and 0 <= nx < C:
-                    if area[ny][nx] == -1: # 얼음을 녹임
+                    if area[ny][nx] == -1: # 주변 얼음을 녹임
                         area[ny][nx] = area[y][x]
-                        nxtMelt.append([ny, nx])
-                    elif area[y][x] != area[ny][nx]:
-                        idx1, idx2 = area[y][x], area[ny][nx]
-                        joint(idx1, idx2)
-        for i in range(R):
-            print(area[i])
-        print('rootIdx: %s' %rootIdx)
-        print('-----------------------')
+                        nxtMelt.append([ny, nx]) # 가장자리에 추가
+                        for k in range(4): # 녹은 위치 주변에 다른 idx가 있는지 확인
+                            ny2, nx2 = ny + dy[i], nx + dx[i]
+                            if 0 <= ny2 < R and 0 <= nx2 < C and area[ny2][nx2] != -1: # 다른 area 만났음
+                                if findroot(area[ny][nx]) != findroot(area[ny2][nx2]):
+                                    joint(area[ny][nx], area[ny2][nx2])
+                    else: # 주변 물과 합치는 과정 수행
+                        if findroot(area[y][x]) != findroot(area[ny][nx]):
+                            joint(area[y][x], area[ny][nx])
         melting = [nxtMelt[i] for i in range(len(nxtMelt))]
         t += 1
 
 
-def joint(idx1, idx2):
+def joint(idx1, idx2): # idx1 과 idx2를 합침
     global rootIdx
-    if rootIdx[idx1] != rootIdx[idx2]: # 서로 다른 그룹일 때
-        rootIdx[idx2] = rootIdx[idx1]
+    root1, root2 = findroot(idx1), findroot(idx2)
+    if root1 != root2: # 서로 다른 그룹일 때
+        rootIdx[root2] = rootIdx[root1]
 
 
 def findroot(areaI): # area idx의 root를 구함
