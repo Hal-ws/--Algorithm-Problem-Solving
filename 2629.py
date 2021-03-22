cd613bbba53c0c7fd2650e84c00961ce243a992e
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-03-22 14:55:21 +0900

    Update 2629.py

--- 2629.py
+++ 2629.py
@@ -6,21 +6,24 @@ def main():
     wList = list(map(int, sys.stdin.readline().split()))
     bCnt = int(sys.stdin.readline())
     bList = list(map(int, sys.stdin.readline().split()))
-    dp = [[0 for j in range(50001)] for i in range(wCnt + 1)]
+    dp = [[0 for j in range(15001)] for i in range(wCnt + 1)]
     for i in range(1, wCnt + 1): # i: dp의 행
         w = wList[i - 1] # 지금 무게
         dp[i][w] = 1
-        for j in range(50001):
+        for j in range(15001):
             if dp[i - 1][j]: # i - 1행에서 무게 j 인 값이 가능할 때
                 dp[i][j] = dp[i - 1][j]
                 dp[i][abs(w - j)] = 1
                 dp[i][j + w] = 1
     for i in range(bCnt):
         bW = bList[i]
-        if dp[wCnt][bW]:
-            print('Y', end=' ')
-        else:
+        if bW > 15000:
             print('N', end=' ')
+        else:
+            if dp[wCnt][bW]:
+                print('Y', end=' ')
+            else:
+                print('N', end=' ')
 
 
 if __name__ == '__main__':
