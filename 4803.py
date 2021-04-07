98555d89182d02590845e74db3c7ceec7add43ac
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-04-07 15:26:41 +0900

    Update 4803.py

--- 4803.py
+++ 4803.py
@@ -3,14 +3,15 @@ import sys
 
 def main():
     global roots
-    t = 1
+    t = 0
     while 1:
         n, m = map(int, sys.stdin.readline().split())
+        t += 1
         if n == 0 and m == 0:
             break
         roots = [i for i in range(n + 1)]
         trees = set() # trees의 root가 되는것들을 저장
-        for i in range(m):
+        for _ in range(m):
             node1, node2 = map(int, sys.stdin.readline().split())
             root1, root2 = find(node1), find(node2)
             if root1 != -1 and root2 != -1: # 둘다 tree의 root일때
@@ -18,16 +19,16 @@ def main():
                     union(root1, root2)
                 else: # 서로 같을때. 사이클을 이룸
                     roots[root1] = -1
-            else:
-                if root1 == -1: # root1이 -1일 때
                     roots[root2] = -1
-                if root2 == -1:
+            else: # cycle의 node를 골랐을 때
+                if root1 == -1 and root2 != -1:
+                    roots[root2] = -1
+                if root1 != -1 and root2 == -1:
                     roots[root1] = -1
         for i in range(1, n + 1):
-            if roots[i] != -1:
-                root = find(i)
-                if root != -1:
-                    trees.add(root)
+            root = find(i)
+            if root != -1:
+                trees.add(root)
         cnt = len(list(trees))
         if cnt == 0:
             print('Case %s: No trees.' %t)
@@ -35,7 +36,6 @@ def main():
             print('Case %s: There is one tree.'%t)
         else:
             print('Case %s: A forest of %s trees.' %(t, cnt))
-        t += 1
 
 
 def union(node1, node2):
@@ -47,10 +47,10 @@ def union(node1, node2):
 
 def find(node):
     global roots
-    if roots[node] == node:
-        return node
     if roots[node] == -1:
         return -1
+    if roots[node] == node:
+        return node
     roots[node] = find(roots[node])
     return roots[node]
 
