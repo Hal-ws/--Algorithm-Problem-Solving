c93333772f3e7ce16cd6732fddbc1610fd7a1972
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-04-16 12:18:00 +0900

    Update 1194.py

--- 1194.py
+++ 1194.py
@@ -1,5 +1,6 @@
 import sys
 from _collections import deque
+from math import inf
 
 
 def main():
@@ -7,7 +8,7 @@ def main():
     N, M = map(int, sys.stdin.readline().split())
     board = []
     dy, dx = [-1, 1, 0, 0], [0, 0, -1, 1]
-    ans = -1
+    ans = inf
     minsik = None
     for i in range(N):
         board.append(list(sys.stdin.readline()[:M]))
@@ -15,51 +16,43 @@ def main():
             if board[i][j] == '0':
                 minsik = [i, j] #민식이 좌표
                 board[i][j] = '.'
-    key = [0, 0, 0, 0, 0, 0]
-    bfs(minsik, key, 0)
-    print(ans)
+    bfs(minsik)
+    if ans == inf:
+        print(-1)
+    else:
+        print(ans)
 
 
-def bfs(start, key, cnt):
+def bfs(start):
     global N, M, board, dy, dx, ans
-    if ans != -1 and cnt >= ans:
-        return
+    visit = [[[0 for k in range(64)] for j in range(M)] for i in range(N)]
     q = deque()
-    q.append([start[0], start[1], cnt])
-    visit = [[0 for j in range(M)] for i in range(N)]
-    visit[start[0]][start[1]] = 1
+    q.append([start[0], start[1], 0, 0]) # y, x, nxtKey(10진수로 저장)
+    visit[start[0]][start[1]][0] = 1
     while len(q) > 0:
-        print('q: %s' %q)
-        print('key: %s' %key)
         tmp = q.popleft()
-        y, x, mCnt = tmp[0], tmp[1], tmp[2]
+        y, x, curKey, cnt = tmp[0], tmp[1], tmp[2], tmp[3]
         for i in range(4):
             ny, nx = y + dy[i], x + dx[i]
-            if 0 <= ny < N and 0 <= nx < M and visit[ny][nx] == 0:
-                if board[ny][nx] != '#': # 벽이 아니면 진행
-                    iVal = ord(board[ny][nx])
-                    visit[ny][nx] = 1
-                    if 97 <= iVal <= 102: #key를 찾았을 때
-                        if key[iVal - 97] == 0: # 키를 새로 주울 때
-                            key[iVal - 97] = 1
-                            board[ny][nx] = '.'
-                            bfs([ny, nx], key, mCnt + 1)
-                            key[iVal - 97] = 0
-                            board[ny][nx] = chr(iVal)
-                        else: #이미 그 키가 있을 때
-                            q.append([ny, nx, mCnt + 1])
-                    if 65 <= iVal <= 70: # 문일때
-                        if key[iVal - 65]: # 키가 있을 때
-                            q.append([ny, nx, mCnt + 1])
-                    if iVal == 46: # 빈칸
-                        q.append([ny, nx, mCnt + 1])
-                    if iVal == 49:
-                        if ans == -1 or mCnt + 1 < ans:
-                            ans = mCnt + 1
-                        return
-        if ans != -1 and cnt >= ans:
-            return
-    return
+            if 0 <= ny < N and 0 <= nx < M and board[ny][nx] != '#':
+                if (board[ny][nx] == '.' or board[ny][nx] == '0') and visit[ny][nx][curKey] == 0:
+                    if visit[ny][nx][curKey] == 0:
+                        visit[ny][nx][curKey] = 1
+                        q.append([ny, nx, curKey, cnt + 1])
+                if 97 <= ord(board[ny][nx]) <= 102: # nxtKey get
+                    nxtKey = curKey | pow(2, ord(board[ny][nx]) - 97)
+                    if visit[ny][nx][nxtKey] == 0:
+                        visit[ny][nx][nxtKey] = 1
+                        q.append([ny, nx, nxtKey, cnt + 1])
+                if 65 <= ord(board[ny][nx]) <= 70:
+                    cmp = curKey | pow(2, ord(board[ny][nx]) - 65)
+                    if curKey == cmp: # 이동가능함
+                        if visit[ny][nx][curKey] == 0:
+                            visit[ny][nx][curKey] = 1
+                            q.append([ny, nx, curKey, cnt + 1])
+                if board[ny][nx] == '1':
+                    if cnt + 1 < ans:
+                        ans = cnt + 1
 
 
 if __name__ == '__main__':
