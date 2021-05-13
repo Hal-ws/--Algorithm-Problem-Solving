0ff4afa124b659f4556aabf81ea19a8172701e07
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-05-13 17:08:32 +0900

    Create 1089.py

---
+++ 1089.py
@@ -0,0 +1,59 @@
+import sys
+from itertools import product
+
+
+def main():
+    global numBoard, mem
+    N = int(sys.stdin.readline())
+    w = 4 * N - 1
+    bCases = list(product(['.', '#'], repeat=15))# 가능한 모든 boards의 case
+    numBoard = ["####.##.##.####", "..#..#..#..#..#", "###..#####..###", "###..####..####", "#.##.####..#..#",
+                "####..###..####", "####..####.####", "###..#..#..#..#", "####.#####.####", "####.####..####"]
+    inputBoard = []
+    for _ in range(5):
+        inputBoard.append(sys.stdin.readline()[:w])
+    mem = {}
+    ans = 0
+    cnt = 0 # 가능한 수
+    for case in bCases:
+        chk(case) #현재 case에서 가능한 숫자를 mem에 추가한다
+    # 하나라도 가능한 수를 못찾으면 ans = -1
+    for j in range(0, w, 4):
+        digit = N - 1 - (j // 3)
+        key = ''
+        print('digit: %s' %digit)
+        for i in range(5):
+            key += (inputBoard[i][j] + inputBoard[i][j + 1] + inputBoard[i][j + 2])
+        if mem[key] == -1:
+            ans = -1
+            break
+        else:
+            ans += mem[key] * pow(10, digit)
+    if ans == -1:
+        print(ans)
+    else:
+        print(ans / cnt)
+
+
+def chk(case):
+    global numBoard, mem
+    key = '' #해당 key로 만들수 있는거 확인
+    for i in range(15):
+        key += case[i]
+    mem[key] = -1
+    for i in range(10): #0~10까지 가능한것 확인
+        std = numBoard[i]
+        flag = 1 # case가 std가 될 수 있는지 확인
+        for j in range(15):
+            if std[j] == '.' and key[j] == '#': # 꺼져 있어야 하는데 켜져있음
+                flag = 0
+                break
+        if flag:
+            if mem[key] == -1:
+                mem[key] = i
+            else:
+                mem[key] += i
+
+
+if __name__ == '__main__':
+    main()
