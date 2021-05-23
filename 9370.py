3f6e8c77b973c81afb16f31bef98eed0b87261c6
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-05-24 00:35:57 +0900

    Update 9370.py

--- 9370.py
+++ 9370.py
@@ -10,7 +10,6 @@ def main():
         n, m, t = map(int, sys.stdin.readline().split()) # 도시 수, 도로 수, 목적지 후보 수
         s, g, h = map(int, sys.stdin.readline().split())
         connect = [[] for i in range(n + 1)]
-        strg, strh = str(g), str(h)
         for i in range(m):
             a, b, d = map(int, sys.stdin.readline().split())
             connect[a].append([b, d])
@@ -19,34 +18,36 @@ def main():
         for i in range(t):
             targets.append(int(sys.stdin.readline()))
         targets.sort()
-        path = daikstra(n, connect, s) # targets 에 있는 path들 중 g-h or h-g 가 있는지 확인해 봐야한다
+        path = daikstra(n, connect, s, g, h) # targets 에 있는 path들 중 g-h or h-g 가 있는지 확인해 봐야한다
         for t in targets:
-            tPath = path[t]
-            flag = 0
-            for i in range(len(tPath) - 1):
-                if (tPath[i] == strg and tPath[i + 1] == strh) or (tPath[i] == strh and tPath[i + 1] == strg):
-                    flag = 1
-                    break
-            if flag:
+            if path[t][1]:
                 print(t, end=' ')
 
 
-def daikstra(n, connect, start):
+def daikstra(n, connect, start, g, h):
     distance = [inf for i in range(n + 1)] #
-    path = ['' for i in range(n + 1)] # i번 도시에 도착하기 위한 path 저장
+    path = [['', 0] for i in range(n + 1)] # i번 도시에 도착하기 위한 path 저장. g - h 또는 h - g를 포함하는지 표시
     distance[start] = 0
     heap = []
-    heapq.heappush(heap, [0, start, 0])
-    path[start] = str(start)
+    heapq.heappush(heap, [0, start])
+    path[start][0] = str(start)
     while len(heap) > 0:
         tmp = heapq.heappop(heap)
         cDis, curP = tmp[0], tmp[1]
         for nxt in connect[curP]:
             nxtP, nxtD = nxt[0], nxt[1]
-            if cDis + nxtD < distance[nxtP]:
+            flag = 0
+            if (curP == g and nxtP == h) or (curP == h and nxtP == g) or path[curP][1]:
+                flag = 1
+            if cDis + nxtD < distance[nxtP]: # nxtP로 가는 path 업데이트
                 distance[nxtP] = cDis + nxtD
-                path[nxtP] = path[curP] + str(nxtP)
+                path[nxtP] = [path[curP][0] + str(nxtP), flag]
                 heapq.heappush(heap, [cDis + nxtD, nxtP])
+            if cDis + nxtD == distance[nxtP]:
+                if path[nxtP][1] == 0 and flag:
+                    distance[nxtP] = cDis + nxtD
+                    path[nxtP] = [path[curP][0] + str(nxtP), flag]
+                    heapq.heappush(heap, [cDis + nxtD, nxtP])
     return path
 
 
