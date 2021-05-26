c580ab5408829396e361bc06572e32942196a7a7
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-05-26 16:44:34 +0900

    Update 4902.py

--- 4902.py
+++ 4902.py
@@ -2,52 +2,52 @@ import sys
 
 
 def main():
-    cnt = 1
+    t = 1
     while 1:
-        nList = list(map(int, sys.stdin.readline().split()))
-        layer = nList[0]
+        tList = list(map(int, sys.stdin.readline().split()))
+        layer = tList[0]
+        ans = -1 * (400 * 799 * 1000) - 1
         if layer == 0:
             break
-        else:
-            triangle = [[0 for j in range(layer * 2 - 1)] for i in range(layer)]
-            nIdx = 1
-            maxVal = -160000001
-            for i in range(layer):
-                for j in range(layer - i - 1, layer + i):
-                    triangle[i][j] = nList[nIdx]
-                    nIdx += 1
-            for i in range(1, layer + 1):
-                if i == 1:
-                    tmp = max(nList[1:])
-                elif i == layer:
-                    tmp = sum(nList[1:])
+        tList = tList[1:]
+        triangle = [[0 for j in range(2 * layer - 1)] for i in range(layer)]
+        sTriangle = [[0 for j in range(2 * layer - 1)] for i in range(layer)]
+        for i in range(layer):
+            start = pow(i, 2)
+            for j in range(start, start + 2 * i + 1):
+                triangle[i][j - start] = tList[j]
+                if j == start:
+                    sTriangle[i][j - start] = tList[j]
                 else:
-                    tmp = getsum(triangle, i, layer)
-                if tmp >= maxVal:
-                    maxVal = tmp
-        print('%s. %s' %(cnt, maxVal))
-        cnt += 1
-
-
-def getsum(tri, size, layer):
-    result = -160000001
-    for i in range(layer - size + 1):
-        for j in range(layer - i - 1, layer + i):
-            tmp = 0
-            for k in range(i + size):
-                for l in range(j - (k - i), j + (k - i) + 1):
-                    tmp += tri[k][l]
-            if result <= tmp:
-                result = tmp
-    for i in range(size - 1, layer - size + 1):
-        for j in range(layer - i, layer + i - (size * 2 - 1) + 1):
-            tmp = 0
-            for k in range(i, i + size):
-                for l in range(j + (k - i), j + (size * 2 - 1) - (k - i)):
-                    tmp += tri[k][l]
-            if result <= tmp:
-                result = tmp
-    return result
+                    sTriangle[i][j - start] = tList[j] + sTriangle[i][j - start - 1]
+        # 정방향
+        for i in range(layer):
+            for j in range(0, 2 * i + 1, 2):
+                tmpVal = 0
+                print('i, j: %s, %s' %(i, j))
+                for size in range(1, layer - i + 1):
+                    print('size: %s' %size)
+                    if j == 0:
+                        tmpVal += sTriangle[i + size - 1][(size - 1) * 2]
+                    else:
+                        tmpVal += (sTriangle[i + size - 1][j + (size - 1) * 2] - sTriangle[i + size - 1][j - 1])
+                    if tmpVal > ans:
+                        ans = tmpVal
+                    print('tmpVal: %s' %tmpVal)
+        # 역방향
+        for i in range(layer -1, 2, -1): # i 가 3보다는 같거나 커야 역방향 삼각형이 있을 수 있음
+            for j in range(3, 2 * i + 1, 2):
+                tmpVal = triangle[i][j]
+                if i % 2 == 0:
+                    maxSize = i // 2
+                else:
+                    maxSize = (i // 2) + 1
+                for size in range(2, maxSize + 1):
+                    tmpVal += (sTriangle[i - size + 1][j] - sTriangle[i - size + 1][j - (size * 2 - 1)])
+                if tmpVal > ans:
+                    ans = tmpVal
+        print('%s. %s' %(t, ans))
+        t += 1
 
 
 if __name__ == '__main__':
