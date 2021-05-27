5a59cec3facdd40e1af2c540b1d05e0508d2c167
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-05-27 15:47:43 +0900

    Update 15670.py

--- 15670.py
+++ 15670.py
@@ -1,31 +1,41 @@
 import sys
-from copy import deepcopy
 
 def main():
     N, M = map(int, sys.stdin.readline().split())
     road = list(map(int, sys.stdin.readline().split()))
-    for i in range(M):
-        todayRoad = deepcopy(road)
+    sCnt = getCntList(road)
+    road.reverse()
+    rCnt = getCntList(road)
+    for _ in range(M):
         start, end = map(int, sys.stdin.readline().split())
-        temp = todayRoad[start - 1:end]
-        temp.reverse()
-        todayRoad = todayRoad[:start - 1] + temp + todayRoad[end:]
-        print(countuplen(N, todayRoad))
+        start, end = start - 1, end - 1
+        if start == end:
+            print(sCnt[N - 1])
+        else:
+            rStart, rEnd = N - end - 1, N - start - 1 #
+            cnt = rCnt[rEnd] - rCnt[rStart] + 1
+            if start != 0:
+                cnt += sCnt[start - 1]
+                if road[start - 1] > road[end]:
+                    cnt += 1
+            if end != N - 1:
+                cnt += (sCnt[N - 1] - sCnt[end + 1] + 1)
+                if road[end + 1] < road[start]:
+                    cnt += 1
+            print(cnt)
 
 
-def countuplen(N, todayRoad):
-    cnt, start = 0, 0
-    if N < 2:
-        return 1
-    while start < N - 1:
-        cnt += 1
-        for i in range(start + 1, N):
-            if todayRoad[i] < todayRoad[i - 1]:
-                break
-        start = i
-    if todayRoad[N - 1] < todayRoad[N - 2]:
-        cnt += 1
-    return cnt
+def getCntList(road):
+    l = len(road)
+    cntList = [0 for i in range(l)]
+    cntList[0] = 1
+    for i in range(1, l):
+        if road[i - 1] < road[i]:
+            cntList[i] = cntList[i - 1]
+        else:
+            cntList[i] = cntList[i - 1] + 1
+    return cntList
+
 
 if __name__ == "__main__":
     main()
