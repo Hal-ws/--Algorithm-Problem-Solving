b23b4100e68600fdb1bbcbfe5195270278ea8542
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-05-28 21:33:25 +0900

    Update 2658.py

--- 2658.py
+++ 2658.py
@@ -1,75 +1,104 @@
 import sys
+from collections import deque
+from itertools import combinations
 
 
 def main():
-    board = []
-    dy = [-1, -1, 0, 1, 1, 1, 0, -1]
-    dx = [0, 1, 1, 1, 0, -1, -1, -1]
-    for _ in range(10):
-        board.append(list(sys.stdin.readline()[:10]))
-    visit = [[0 for j in range(10)] for i in range(10)]
-    for i in range(10):
-        flag = 0
-        for j in range(10):
-            if board[i][j] == '1':
-                edges = [[i, j]]
-                visit[i][j] = 1
-                flag = 1
-                break
-        if flag:
-            y, x = edges[0][0], edges[0][1]
-            d = None
-            for j in range(8):
-                ny, nx = y + dy[j], x + dx[j]
-                if 0 <= ny < 10 and 0 <= nx < 10 and board[ny][nx] == '1':
-                    d = j
-                    break
-            if d == None:
-                print(0)
-                return
-            break
-    while 1: # 방향을 가지고 출발
-        # 현재 방향으로 진행하는 다음 좌표
-        print('y, x: %s, %s' %(y, x))
-        for i in range(10):
-            print(visit[i])
-        print('----------------')
-        ny, nx = y + dy[d], x + dx[d]
-        endFlag = 1 # 더이상 진행 가능한 곳을 못찾으면 1 그대로 남는다
-        # 지금 방향 그대로 진행 가능할때
-        if 0 <= ny < 10 and 0 <= nx < 10 and board[ny][nx] == '1':
-            if visit[ny][nx] == 0:
-                visit[ny][nx] = 1
-                y, x = ny, nx
-                endFlag = 0
-            else:
-                break
-        else: # 진행 불가. 꺾어줘야함 (현재 위치인 y, x 가 꼭지점)
-            cFlag = 0
-            for i in range(d + 1, 8):
-                ny, nx = y + dy[i], x + dx[i]
-                if 0 <= ny < 10 and 0 <= nx < 10 and board[ny][nx] == '1' and visit[ny][nx] == 0: # 꺾일수 있는 점인거 확인
-                    visit[ny][nx] = 1
-                    edges.append([y, x])
-                    y, x = ny, nx
-                    cFlag = 1
-                    endFlag = 0
-                    d = i
-                    break
-            if cFlag == 0:
-                for i in range(d):
-                    ny, nx = y + dy[i], x + dx[i]
-                    if 0 <= ny < 10 and 0 <= nx < 10 and board[ny][nx] == '1' and visit[ny][nx] == 0:
-                        visit[ny][nx] = 1
-                        edges.append([y, x])
-                        y, x = ny, nx
-                        endFlag = 0
-                        d = i
-                        break
-        if endFlag:
-            break
-    print(edges)
-
-
-if __name__ == '__main__':
-    main()
+	board = ['000000000000']
+	for _ in range(10):
+		board.append('0' + sys.stdin.readline()[:10] + '0')
+	upperPoint, downPoint,leftPoint, rightPoint = [], [], [], []
+	board.append('000000000000')
+	edges = []
+	for i in range(10):
+		flag = 0
+		for j in range(10):
+			if board[i][j] == '1':
+				upperPoint.append([i, j])
+				flag = 1
+		if flag:
+			break
+	for i in range(9, -1, -1):
+		flag = 0
+		for j in range(10):
+			if board[i][j] == '1':
+				downPoint.append([i, j])
+				flag = 1
+		if flag:
+			break
+	for j in range(10):
+		flag = 0
+		for i in range(10):
+			if board[i][j] == '1':
+				leftPoint.append([i, j])
+				flag = 1
+		if flag:
+			break
+	for j in range(9, -1, -1):
+		flag = 0
+		for i in range(10):
+			if board[i][j] == '1':
+				rightPoint.append([i, j])
+				flag = 1
+		if flag:
+			break
+	if upperPoint[0] not in edges:
+		edges.append(upperPoint[0])
+	if upperPoint[-1] not in edges:
+		edges.append(upperPoint[-1])
+	if downPoint[0] not in edges:
+		edges.append(downPoint[0])
+	if downPoint[-1] not in edges:
+		edges.append(downPoint[-1])
+	if leftPoint[0] not in edges:
+		edges.append(leftPoint[0])
+	if leftPoint[-1] not in edges:
+		edges.append(leftPoint[-1])
+	if rightPoint[0] not in edges:
+		edges.append(rightPoint[0])
+	if rightPoint[-1] not in edges:
+		edges.append(rightPoint[-1])
+	edges.sort()
+	if len(edges) != 3:
+		print(0)
+		return
+	y1, x1, y2, x2, y3, x3 = edges[0]0], edges[0][1], edges[1][0], edges[1][1], edges[2][0], edges[2][1]
+	l1, l2, l3 = pow(x2 - x1, 2) + pow(y2 - y1, 2), pow(x3 - x1, 2) + pow(y3 - y1, 2), pow(x3 - x2, 2) + pow(y3 - y2, 2)
+	lenList = sorted([l1, l2, l3])
+	if lenList[0] + lenList[1] != lenList[2] or lenList[0] != lenList[1]:
+		print(0)
+		return
+	for p in edges:
+		print('%s %s' %(p[0], p[1]))
+	cases = list(combinations([0, 1, 2], 2))
+	nBoard = [[0 for j in range(12)] for i in range(12)] # draw lines in this board
+	for case in cases:
+		p1, p2 = edges[case[0]], edges[case[1]]
+		y1, x1, y2, x2 = p1[0], p1[1], p2[0], p2[1]
+		if x1 == x2: #vertical
+			for y in range(y1, y2 + 1):
+				nBoard[y][x1] = 1
+				if board[y][x1] != '1': # empty place is existing
+					print(0)
+					return
+		else:
+			m = (y2 - y1) / (x2 - x1)
+			if m == 0 or m == 1 or m == -1:
+				m = int(m)
+				d = 0
+				for x in range(x1, x2 + 1):
+					 
+			else:
+				print(0)
+				return
+			 
+		
+		
+	
+		
+def bfs(start, board, color):
+		
+			
+	
+if __name__ == "__main__":
+	main()
