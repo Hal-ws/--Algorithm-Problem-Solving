858eee1bdaddbef8ce369e35a53c998b039a2600
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-06-13 22:17:29 +0900

    Update 16933.py

--- 16933.py
+++ 16933.py
@@ -1,5 +1,5 @@
 import sys
-from collections import deque
+from _collections import deque
 
 
 def main():
@@ -8,59 +8,44 @@ def main():
     board = []
     for i in range(N):
         board.append(list(sys.stdin.readline()[:M]))
+        for j in range(M):
+            board[i][j] = int(board[i][j])
     print(bfs())
 
 
 def bfs():
     global N, M, K, board
-    dy, dx = [-1, 1, 0, 0, 0], [0, 0, -1, 1, 0]
-    visit = [[[[0 for j in range(M)] for i in range(N)] for k in range(K + 1)] for l in range(2)]
-    visit[0][K][0][0] = 1
+    dy = [-1, 1, 0, 0]
+    dx = [0, 0, -1, 1]
+    visit = [[[0 for j in range(M)] for i in range(N)] for k in range(K + 1)]
+    breakableCnt = [[0 for j in range(M)] for i in range(N)]
     q = deque()
-    q.append([0, K, 0, 0, 1]) #밤낮, 부술수 있는벽, y, x, 방문 칸 수
+    q.append([0, 0, K, 0, 1]) # y, x, 부술수 있는것, 날짜, 방문한 칸의 수
+    visit[K][0][0] = 1
+    breakableCnt[0][0] = K
     while len(q) > 0:
-        time, possK, y, x, cnt = q[0][0], q[0][1], q[0][2], q[0][3], q[0][4]
+        print('q: %s' %q)
+        tmp = q.popleft()
+        y, x, possK, time, cnt = tmp[0], tmp[1], tmp[2], tmp[3], tmp[4]
+        nxtD = (time + 1) % 2
         if y == N - 1 and x == M - 1:
             return cnt
-        if time == 0: # 낮일때
-            for i in range(4): # 멈춰있을 이유 없음
-                ny, nx = y + dy[i], x + dx[i]
-                if 0 <= ny < N and 0 <= nx < M:
-                    if board[ny][nx] == '1' and 0 < possK: #벽을 부수고 갈때
-                        if visit[1][possK - 1][ny][nx] == 0:
-                            visit[1][possK - 1][ny][nx] = cnt + 1
-                            q.append([1, possK - 1, ny, nx, cnt + 1])
-                        elif cnt + 1 < visit[1][possK - 1][ny][nx]:
-                            visit[1][possK - 1][ny][nx] = cnt + 1
-                            q.append([1, possK - 1, ny, nx, cnt + 1])
-                    if board[ny][nx] == '0': # 안부수고 이동가능
-                        if visit[1][possK][ny][nx] == 0:
-                            visit[1][possK][ny][nx] = cnt + 1
-                            q.append([1, possK, ny, nx, cnt + 1])
-                        elif cnt + 1 < visit[1][possK][ny][nx]:
-                            visit[1][possK][ny][nx] = cnt + 1
-                            q.append([1, possK, ny, nx, cnt + 1])
-        else: # 밤일때
-            for i in range(5): # 멈추고 가는게 나을수도 있음
-                ny, nx = y + dy[i], x + dx[i]
-                if 0 <= ny < N and 0 <= nx < M:
-                    if board[ny][nx] == '0': # 벽으로는 못감
-                        if visit[0][possK][ny][nx] == 0:
-                            visit[0][possK][ny][nx] = cnt + 1
-                            q.append([0, possK, ny, nx, cnt + 1])
-                        elif cnt + 1 < visit[0][possK][ny][nx]:
-                            visit[0][possK][ny][nx] = cnt + 1
-                            q.append([0, possK, ny, nx, cnt + 1])
-                    else: #벽일때
-                        if ny == y and nx == x:
-                            if visit[0][possK][ny][nx] == 0:
-                                visit[0][possK][ny][nx] = cnt + 1
-                                q.append([0, possK, ny, nx, cnt + 1])
-                            elif cnt + 1 < visit[0][possK][ny][nx]:
-                                visit[0][possK][ny][nx] = cnt + 1
-                                q.append([0, possK, ny, nx, cnt + 1])
-        q.popleft()
-    return - 1
+        for i in range(4):
+            ny, nx = y + dy[i], x + dx[i]
+            if 0 <= ny < N and 0 <= nx < M:
+                if board[ny][nx] == 0 and visit[possK][ny][nx] == 0:
+                    if breakableCnt[ny][nx] == 0 or breakableCnt[ny][nx] < possK:
+                        visit[possK][ny][nx] = 1
+                        breakableCnt[ny][nx] = possK
+                        q.append([ny, nx, possK, nxtD, cnt + 1])
+                if board[ny][nx] == 1 and time == 0 and possK > 0 and visit[possK - 1][ny][nx] == 0:
+                    if breakableCnt[ny][nx] == 0 or breakableCnt[ny][nx] < possK:
+                        visit[possK - 1][ny][nx] = 1
+                        breakableCnt[ny][nx] = possK
+                        q.append([ny, nx, possK - 1, nxtD, cnt + 1])
+        if time == 1: # 밤일때는 하루 더 있어봐야함
+            q.append([y, x, possK, nxtD, cnt + 1])
+    return -1
 
 
 if __name__ == '__main__':
