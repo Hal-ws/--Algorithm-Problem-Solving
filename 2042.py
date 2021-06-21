c432fcad21c7cdf86a0776a2fbcea0858543a200
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-06-21 16:39:43 +0900

    Update 2042.py

--- 2042.py
+++ 2042.py
@@ -7,21 +7,27 @@ def main():
     while 1:
         if pow(2, i) >= N:
             tree = [0] * pow(2, i + 1)
+            coverIdx = [[-1, -1] for j in range(pow(2, i + 1))]
             sIdx = pow(2, i) - 1
             break
         i += 1
     for i in range(1, N + 1):
         val = int(sys.stdin.readline())
         tree[sIdx + i] = val
-    print(tree)
+    for i in range(sIdx + 1, len(coverIdx)):
+        coverIdx[i] = [i, i]
     for idx in range(len(tree) - 1, 1, -1):
         tree[idx // 2] += tree[idx]
-    print(tree)
+        if idx % 2 == 1:
+            coverIdx[idx // 2][1] = coverIdx[idx][1]
+        else:
+            coverIdx[idx // 2][0] = coverIdx[idx][0]
     for i in range(K):
         a, b, c = map(int, sys.stdin.readline().split())
         if a == 1:
             changeNum(sIdx + b, c - tree[sIdx + b], tree)
-        print(tree)
+        else:
+            print(getSum(b, c, tree, coverIdx))
 
 
 def changeNum(idx, diff, tree):
@@ -30,7 +36,8 @@ def changeNum(idx, diff, tree):
         idx = idx // 2
 
 
-def getSum(start, end, tree):
+def getSum(start, end, tree, coverIdx):
+
     return 0
 
 
