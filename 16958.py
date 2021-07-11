bd92d91729181b4b8ffb5a5220ecb286e30af9bb
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-07-11 23:35:27 +0900

    Update 16958.py

--- 16958.py
+++ 16958.py
@@ -2,8 +2,18 @@ import sys
 
 
 def main():
+  N, T = map(int, sys.stdin.readline().split())
+  for _ in range(N):
+    s, x, y = map(int, sys.stdin.readline().split())
+  M = int(sys.stdin.readline())
+  for _ in range(M):
+    A, B = map(int, sys.stdin.readline().split())
+    print(getMintime(A, B))
   return 0
 
+
+def getMinTime():
+  return 0
 
 if __name__ == "__main__":
   main()
