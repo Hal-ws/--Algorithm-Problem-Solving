cca5394b0676383222db23c9c9d0c41f953015d3
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-07-14 15:52:00 +0900

    Update 16958.py

--- 16958.py
+++ 16958.py
@@ -1,19 +1,32 @@
 import sys
+from math import inf
 
 
 def main():
-  N, T = map(int, sys.stdin.readline().split())
-  for _ in range(N):
-    s, x, y = map(int, sys.stdin.readline().split())
-  M = int(sys.stdin.readline())
-  for _ in range(M):
-    A, B = map(int, sys.stdin.readline().split())
-    print(getMintime(A, B))
-  return 0
+    N, T = map(int, sys.stdin.readline().split())
+    cityInfo = [[] for i in range(N + 1)]
+    disMatrix = [[inf for j in range(N + 1)] for i in range(N + 1)]
+    for i in range(1, N + 1):
+        cityInfo[i] = list(map(int, sys.stdin.readline().split()))
+    M = int(sys.stdin.readline())
+    for i in range(1, N - 1):
+        for j in range(i + 1, N):
+            city1, city2 = cityInfo[i], cityInfo[j]
+            dis1 = abs(city1[1] - city2[1]) + abs(city1[2] - city2[2])
+            if dis1 < disMatrix[i][j]:
+                disMatrix[i][j] = dis1
+            if dis1 < disMatrix[j][i]:
+                disMatrix[j][i] = dis1
+            if city1[0] == city2[0] == 1:
+                if T < disMatrix[i][j]:
+                    disMatrix[i][j] = T
+                if T < disMatrix[j][i]:
+                    disMatrix[j][i] = T
+    for i in range(M):
+        a, b = map(int, sys.stdin.readline().split())
 
 
-def getMinTime():
-  return 0
 
-if __name__ == "__main__":
-  main()
+
+if __name__ == '__main__':
+    main()
