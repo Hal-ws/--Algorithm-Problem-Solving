e342ca89a586b9479c19edfd0556208d2f9d0911
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-07-26 23:10:40 +0900

    Create 2650.py

---
+++ 2650.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def main():
+    N = int(sys.stdin.readline())
+    points = []
+    for i in range(N // 2):
+        points.append(list(map(int, sys.stdin.readline().split())))
+
+
+
+if __name__ == "__main__":
+    main()
+        
