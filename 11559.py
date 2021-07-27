08d63fcb18bc42614e7bfccadd2d084557b0c7fe
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-07-27 11:03:59 +0900

    Update 11559.py

--- 11559.py
+++ 11559.py
@@ -3,74 +3,70 @@ from collections import deque
 
 
 def main():
-    global board, dy, dx
-    cnt = 0
     board = []
-    dy, dx = [-1, 1, 0, 0], [0, 0, -1, 1]
+    answer = 0
     for i in range(12):
         board.append(list(sys.stdin.readline()[:6]))
     while 1:
-        flag = 0 #
-        visit = [[0 for j in range(6)] for i in range(12)]
-        for i in range(12):
-            for j in range(6):
-                if visit[i][j] == 0 and board[i][j] != '.':
-                    puyo = bfs([i, j], visit)
-                    if len(puyo) >= 4:
-                        flag = 1 #터뜨릴수 있는거 있는것 확인
-                        for k in range(len(puyo)):
-                            board[puyo[k][0]][puyo[k][1]] = '0' #터짐
-        down()
-        if flag: #터지는게 일어났으면 cnt +
-            cnt += 1
+        val = puyo(board)
+        if val:
+            answer += 1
         else:
             break
-    print(cnt)
+    print(answer)
 
 
-def down():
-    global board
-    for j in range(6):
-        flag, sIdx, eIdx = 0, -1, -1
-        for i in range(11, -1, -1):
-            if board[i][j] == '0':
-                sIdx = i
-                flag = 1
-                break
-        if flag:
-            for i in range(sIdx, -1, -1):
-                if board[i][j] != '0':
-                    eIdx = i + 1
-                    break
-                elif i == 0: # 빈칸이지만 위 끝까지 간경우
-                    eIdx = i
-            dis = sIdx - eIdx + 1
-            for i in range(eIdx - 1, -1, -1):
-                board[i + dis][j] = board[i][j]
-            for i in range(dis):
-                board[i][j] = '.'
+def puyo(board):
+    puyoChk = 0 # puyo가 있었는지 판단
+    for i in range(12):
+        for j in range(6):
+            if board[i][j] != '.':
+                tmp = bfs(i, j, board)
+                if tmp:
+                    puyoChk = 1
+    if puyoChk:
+        falling(board)
+    return puyoChk
 
 
-def bfs(pos, visit):
-    global board, dy, dx
-    y, x = pos[0], pos[1]
-    color = board[y][x]
-    visit[y][x] = 1
+def bfs(sy, sx, board):
+    dy = [-1, 1, 0, 0]
+    dx = [0, 0, -1, 1]
     q = deque()
-    q.append([y, x])
-    result = [[y, x]]
+    color = board[sy][sx]
+    q.append([sy, sx])
+    visit = [[0 for j in range(6)] for i in range(12)]
+    visit[sy][sx] = 1
+    puyoList = [[sy, sx]]
     while len(q) > 0:
-        y, x = q[0][0], q[0][1]
+        tmp = q.popleft()
+        y, x = tmp[0], tmp[1]
         for i in range(4):
             ny, nx = y + dy[i], x + dx[i]
-            if 0 <= ny < 12 and 0 <= nx < 6:
-                if visit[ny][nx] == 0 and board[ny][nx] == color:
-                    visit[ny][nx] = 1
-                    q.append([ny, nx])
-                    result.append([ny, nx])
-        q.popleft()
-    return result
+            if 0 <= ny < 12 and 0 <= nx < 6 and visit[ny][nx] == 0 and board[ny][nx] == color:
+                puyoList.append([ny, nx])
+                visit[ny][nx] = 1
+                q.append([ny, nx])
+    if len(puyoList) >= 4:
+        for pos in puyoList:
+            y, x = pos[0], pos[1]
+            board[y][x] = '.'
+        return 1
+    return 0
 
 
-if __name__ == '__main__':
+def falling(board):
+    for x in range(6):
+        for y in range(11, -1, -1):
+            if board[y][x] != '.': # 빈칸이 아닌 걸 봤을때 밑에 떨어질 공간이 있는지 확인한후 떨어짐
+                tmpY = y
+                for i in range(tmpY + 1, 12):
+                    if board[i][x] == '.': # 떨어질 공간 있음
+                        board[i][x], board[tmpY][x] = board[tmpY][x], board[i][x]
+                        tmpY += 1
+                    else: #떨어질 공간 없으니 넘어감
+                        break
+
+
+if __name__ == "__main__":
     main()
