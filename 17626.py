7f157c8fb1dad91bf422071b0a671fa5e6513cfa
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-07-30 14:53:11 +0900

    Update 17626.py

--- 17626.py
+++ 17626.py
@@ -1,30 +1,25 @@
-from math import sqrt
-
-
 def main():
     N = int(input())
-    if pow(int(sqrt(N)), 2) == N:
-        print(1)
-        return
-    maxN = int(sqrt(N))
-    for i in range(maxN, 1, -1):
-        for j in range(i - 1, 0, -1):
-            if pow(i, 2) + pow(j, 2) == N:
-                print(2)
-                return
-    for i in range(maxN, 2, -1):
-        left = N - pow(i, 2)
-        max2 = int(sqrt(left))
-        for j in range(max2, 1, -1):
-            left2 = left - pow(j, 2)
-            max3 = int(sqrt(left))
-            for k in range(max3, 0, -1):
-                left3 = left2 - pow(k, 2)
-                if left3 == 0:
-                    print(3)
-                    return
-    print(4)
-    return
+    for i in range(N):
+        if i * i <= N:
+            maxVal = i
+        else:
+            break
+    dp = [[0 for j in range(N + 1)] for i in range(5)]
+    for j in range(1, maxVal + 1):
+        dp[1][j * j] = 1
+        if j * j == N:
+            print(1)
+            return
+    for i in range(2, 5): # 사용 숫자 갯수 i - 1인거랑 비교
+        for j in range(N + 1):
+            if dp[i - 1][j]: # 사용갯수가 i - 1개인것 중 값이 j인게 존재할때
+                for k in range(1, maxVal + 1):
+                    if j + k * k <= N:
+                        dp[i][j + k * k] = 1
+                        if j + k * k == N:
+                            print(i)
+                            return
 
 
 if __name__ == '__main__':
