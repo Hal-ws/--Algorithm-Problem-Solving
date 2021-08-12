9ea1b1c9b71dc261635db6094e880015d2115bf9
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-08-12 10:30:04 +0900

    Update 17626.py

--- 17626.py
+++ 17626.py
@@ -1,26 +1,24 @@
+import sys
+from math import sqrt
+
+
 def main():
-    N = int(input())
-    for i in range(N):
-        if i * i <= N:
-            maxVal = i
-        else:
-            break
-    dp = [[0 for j in range(N + 1)] for i in range(5)]
-    for j in range(1, maxVal + 1):
-        dp[1][j * j] = 1
-        if j * j == N:
-            print(1)
-            return
-    for i in range(2, 5): # 사용 숫자 갯수 i - 1인거랑 비교
-        for j in range(N + 1):
-            if dp[i - 1][j]: # 사용갯수가 i - 1개인것 중 값이 j인게 존재할때
-                for k in range(1, maxVal + 1):
-                    if j + k * k <= N:
-                        dp[i][j + k * k] = 1
-                        if j + k * k == N:
-                            print(i)
-                            return
+    N = int(sys.stdin.readline())
+    dp = [0 for i in range(50001)]
+    for i in range(int(sqrt(50001) + 1)):
+        dp[i * i] = 1
+    for i in range(2, 50001):
+        if dp[i] == 0:
+            minVal = 5
+            for j in range(1, i + 1):
+                idx = i - j * j
+                if idx <= 0:
+                    break
+                if dp[idx] < minVal:
+                    minVal = dp[idx]
+            dp[i] = minVal + 1
+    print(dp[N])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
