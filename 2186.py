4accf4fe723d09d0190ed39eb17588432fd612e7
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-08-23 22:18:16 +0900

    Update 2186.py

--- 2186.py
+++ 2186.py
@@ -4,59 +4,49 @@ import sys
 def main():
     N, M, K = map(int, sys.stdin.readline().split())
     board = []
-    aList = [[] for i in range(26)]
-    for i in range(N):
-        board.append(list(sys.stdin.readline()[:M]))
-    target = sys.stdin.readline()
-    l = len(target)
-    target = target[:l - 1]
-    l -= 1
     answer = 0
     for i in range(N):
+        board.append(sys.stdin.readline()[:M])
+    target = sys.stdin.readline()
+    target = target[:len(target) - 1]
+    dp = [[[-1 for j in range(M)] for i in range(N)] for k in range(len(target))] # -1: 아직 계산 안됨
+    for i in range(N):
         for j in range(M):
-            aList[ord(board[i][j]) - 65].append([i, j])
-    dp = [[0 for j in range(M)] for i in range(N)]
-    for sPos in aList[ord(target[0]) - 65]: # 시작 좌표들
-        dfs(sPos, 0, l, dp, target, aList, K)
-    for sPos in aList[ord(target[0]) - 65]:
-        answer += dp[sPos[0]][sPos[1]]
+            if board[i][j] == target[0]:
+                dfs(0, dp, [i, j], board, K, target, N, M)
+    for i in range(N):
+        for j in range(M):
+            if dp[0][i][j] > 0:
+                answer += dp[0][i][j]
+    for i in range(N):
+        print(dp[0][i])
     print(answer)
 
 
-def dfs(pos, idx, l, dp, target, aList, K):
+def dfs(idx, dp, pos, board, K, target, N, M):
     y, x = pos[0], pos[1]
-    if idx == l - 1: # 끝까지 도달함
-        dp[y][x] = 1
-        return 1
-    nxtC = target[idx + 1] # 다음 문자
-    flag = 0
-    for nxtPos in aList[ord(nxtC) - 65]: # 다음 좌표
-        if pickable(pos, nxtPos, K): # 이동 가능한 좌표
-            ny, nx = nxtPos[0], nxtPos[1]
-            if dp[ny][nx] == 0: # 아직 진행 안해본 곳일때 진행
-                tmp = dfs(nxtPos, idx + 1, l, dp, target, aList, K)
-                if tmp > 0:
-                    dp[y][x] += tmp
-                    flag = 1
-            elif dp[ny][nx] > 0: #이미 도달한 값이 있을때. 더이상 진행안함
-                dp[y][x] += dp[ny][nx]
-                flag = 1
-            else: # 불가능한 곳일때. 안감
-                continue
-    if flag == 0:
-        dp[y][x] = -1
-    return dp[y][x]
-
-
-def pickable(p1, p2, K): # 고를 수 있는지 확인
-    y1, x1, y2, x2 = p1[0], p1[1], p2[0], p2[1]
-    if y1 == y2 and x1 == x2: #동일 지점은 선택 불가
-        return 0
-    if y1 == y2 and abs(x2 - x1) <= K:
-        return 1
-    if x1 == x2 and abs(y2 - y1) <= K:
-        return 1
-    return 0
+    if idx == len(target) - 1: # 끝까지 다 도착함
+        dp[idx][y][x] = 1
+    else:
+        nxtChar = target[idx + 1]
+        tmpSum = 0
+        for i in range(y - K, y + K + 1):
+            if 0 <= i < N and i != y:
+                if board[i][x] == nxtChar:
+                    if dp[idx + 1][i][x] > 0:
+                        tmpSum += dp[idx + 1][i][x]
+                    elif dp[idx + 1][i][x] < 0:
+                        tmpSum += dfs(idx + 1, dp, [i, x], board, K, target, N, M)
+        for j in range(x - K, x + K + 1):
+            if 0 <= j < M and j != x:
+                if board[y][j] == nxtChar:
+                    if dp[idx + 1][y][j] > 0: # 이미 결과를 구했고, 가능한 경우일때
+                        tmpSum += dp[idx + 1][y][j]
+                    elif dp[idx + 1][y][j] < 0:
+                        tmpSum += dfs(idx + 1, dp, [y, j], board, K, target, N, M)
+        dp[idx][y][x] = 0
+        dp[idx][y][x] += tmpSum
+    return dp[idx][y][x]
 
 
 if __name__ == '__main__':
