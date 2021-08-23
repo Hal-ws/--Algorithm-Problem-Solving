54e27c29b857c572a028a7f07a2a962db96d59e4
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-08-23 11:19:09 +0900

    Update 2098.py

--- 2098.py
+++ 2098.py
@@ -1,35 +1,36 @@
 import sys
-from math import inf
-from _collections import deque
+from collections import deque
 
 
 def main():
+    global matrix
     N = int(sys.stdin.readline())
     matrix = []
-    ans = inf
+    answer = 1000000 * 16 + 1
     for i in range(N):
         matrix.append(list(map(int, sys.stdin.readline().split())))
-    dp = [[inf for j in range(pow(2, N))] for i in range(N)]
+    dp = [[-1 for j in range(N)] for i in range(pow(2, N))]
     q = deque()
-    for i in range(N):
-        q.append([i, pow(2, i)])
-        dp[i][pow(2, i)] = 0
+    q.append([1, 0]) # path, 현재 node 저장
+    dp[1][0] = 0
     while len(q) > 0:
-        print('q: %s' %q)
         tmp = q.popleft()
-        cur, path = tmp[0], tmp[1]
-        if path == pow(2, N) - 1:
-            if dp[cur][path] < ans:
-                ans = dp[cur][path]
-        else:
-            for nxt in range(N):
-                if matrix[cur][nxt] != 0: # 연결되는 길이 있을 때
-                    if path | pow(2, nxt) == path + pow(2, nxt): #현재 path 에서 nxt번째가 방문 안된거일때
-                        if dp[cur][path] + matrix[cur][nxt] < dp[nxt][path + pow(2, nxt)]: # 더 짧게 방문할 수 있을 때
-                            dp[nxt][path + pow(2, nxt)] = dp[cur][path] + matrix[cur][nxt]
-                            q.append([nxt, path + pow(2, nxt)])
-    print(ans)
-
+        path, cNode = tmp[0], tmp[1]
+        for i in range(N):
+            if matrix[cNode][i] != 0:
+                nxtNode = pow(2, i) # nxtNode를 2진법으로 표현
+                nPath = path | nxtNode
+                cost = dp[path][cNode] + matrix[cNode][i]
+                if nPath != path:
+                    if dp[nPath][i] == -1 or cost < dp[nPath][i]:
+                        dp[nPath][i] = cost
+                        q.append([nPath, i])
+    for i in range(1, N):
+        if matrix[i][0] != 0 and dp[pow(2, N) - 1][i] != -1:
+            cost = dp[pow(2, N) - 1][i] + matrix[i][0]
+            if cost < answer:
+                answer = cost
+    print(answer)
 
 
 if __name__ == '__main__':
