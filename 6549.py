12078325fd5cd6cd729db0b2edf8db5175a423ef
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-09-03 17:17:02 +0900

    Update 6549.py

--- 6549.py
+++ 6549.py
@@ -8,30 +8,35 @@ def main():
             break
         else:
             hList = tmpList[1:]
-        stack = [[0, 0]] # 높이, 위치
+        stack = [[0, 0]] # 위치, 높이
+        areaList = [[0, 0] for i in range(len(hList) + 1)]  # i번 bar로 끝나는 최대 직사각형의 가로 / 세로 길이 표시
         ans = 0
         for i in range(len(hList)):
             p, h = i + 1, hList[i]
-            topH = stack[-1][0]
-            print('stack: %s' %stack)
+            lastP, topH = stack[-1][0], stack[-1][1]
+            print('i: %s' %i)
             if topH < h:
-                if topH == 0:
-                    if ans < h:
-                        ans = h
-                elif ans < topH * (p - stack[-1][1] + 1):
-                    ans = topH * (p - stack[-1][1] + 1)
-            else:
-                tmpSize = 0
+                thisW, thisH = areaList[lastP][0] + (p - lastP), areaList[lastP][1]
+                if thisW * thisH <= h:
+                    areaList[p][0] = 1
+                    areaList[p][1] = h
+                else:
+                    areaList[p][0] = thisW + 1
+                    areaList[p][1] = topH
+            else: # 더 작거나 같은
                 while len(stack) > 0:
-                    if stack[-1][0] < h:
-                        tmpSize = h * (p - stack[-1][1])
-                        break
-                    else:
+                    if h <= stack[-1][1]:  # 현재 직사각형보다 더 작은것만 남겨둠
                         stack.pop()
-                if ans < tmpSize:
-                    ans = tmpSize
-            stack.append([h, p])
-            print('answer: %s' %ans)
+                    else:
+                        break
+                lastP, lastH = stack[-1][0], stack[-1][1]
+
+            stack.append([p, h])
+            print('stack: %s' %stack)
+            print('area: %s' %areaList)
+            thisArea = areaList[i][0] * areaList[i][1]
+            if ans < thisArea:
+                ans = thisArea
         print(ans)
 
 
