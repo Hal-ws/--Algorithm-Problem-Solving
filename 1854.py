26a6b0b8427987d8defb68d574cfbaf26bf1c2ba
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-09-20 23:29:54 +0900

    Update 1854.py

--- 1854.py
+++ 1854.py
@@ -9,7 +9,7 @@ def main():
         a, b, c = map(int, sys.stdin.readline().split())
         connect[a].append([b, c])
     disList = [[] for i in range(n + 1)]
-    disList[1].append([0])
+    disList[1].append(0)
     hq = []
     heapq.heappush(hq, [0, 1])
     while len(hq) > 0:
@@ -19,37 +19,21 @@ def main():
             nxtP, tDis = nxt[0], nxt[1] + d
             l = len(disList[nxtP])
             if l < k: # k개보다 적은 거리들이 있을경우
-                if bSearch(disList[nxtP], tDis) == 0:  # tDis가 없는 경우에만 추가해준다
+                disList[nxtP].append(tDis)
+                disList[nxtP].sort()
+                heapq.heappush(hq, [tDis, nxtP])
+            else:  # 이미 k개가 꽉찬 경우
+                maxDis = disList[nxtP][-1]
+                if tDis <= maxDis: # 최대 거리보다 더 작은 값을 집어넣어야 함
+                    disList[nxtP].pop()
                     disList[nxtP].append(tDis)
                     disList[nxtP].sort()
                     heapq.heappush(hq, [tDis, nxtP])
-            else:  # 이미 k개가 꽉찬 경우
-                maxDis = disList[nxtP][-1]
-                if tDis < maxDis: # 최대 거리보다 더 작은 값을 집어넣어야 함
-                    if bSearch(disList[nxtP], tDis) == 0:
-                        disList[nxtP].pop()
-                        disList[nxtP].append(tDis)
-                        disList[nxtP].sort()
-                        heapq.heappush(hq, [tDis, nxtP])
     for i in range(1, n + 1):
         if len(disList[i]) < k:
             print(-1)
         else:
             print(disList[i][k - 1])
-
-
-def bSearch(nList, val):
-    left, right = 0, len(nList) - 1
-    while left <= right:
-        mid = (left + right) // 2
-        if nList[mid] == val:
-            return 1
-        if nList[mid] < val:
-            left = mid + 1
-        else:
-            right = mid - 1
-    return 0
-
 
 if __name__ == "__main__":
     main()
