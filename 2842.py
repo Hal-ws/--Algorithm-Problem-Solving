49f6e40bedf9c9c4212804246c95b38bae20a2fc
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-10-17 22:29:08 +0900

    Update 2842.py

--- 2842.py
+++ 2842.py
@@ -7,67 +7,73 @@ def main():
     town = []
     for i in range(N):
         town.append(list(sys.stdin.readline()[:N]))
-    hList = []
+    hBoard = []
     for i in range(N):
-        hList.append(list(map(int, sys.stdin.readline().split())))
+        hBoard.append(list(map(int, sys.stdin.readline().split())))
     minH, maxH = 1000001, 0
+    KList = []
     for i in range(N):
         for j in range(N):
             if town[i][j] == 'P':
-                sPos = [i, j]
-            if hList[i][j] <= minH:
-                minH = hList[i][j]
-            if hList[i][j] >= maxH:
-                maxH = hList[i][j]
+                sY, sX = i, j
+            if town[i][j] == 'K':
+                KList.append([i, j])
+            if hBoard[i][j] <= minH:
+                minH = hBoard[i][j]
+            if hBoard[i][j] >= maxH:
+                maxH = hBoard[i][j]
     left, right = 0, maxH - minH
-    ans = 0
+    ans = right
     while left <= right:
         mid = (left + right) // 2
-        if bfs(sPos, town, hList, mid, N):  # mid의 피로도로 이동가능
+        visit = [[0 for j in range(N)] for i in range(N)]
+        visit[sY][sX] = 1
+        print('mid: %s' %mid)
+        if dfs(sY, sX, visit, hBoard[sY][sX], hBoard[sY][sX], hBoard, mid, N, KList):
             ans = mid
             right = mid - 1
         else:
             left = mid + 1
+        visit[sY][sX] = 0
+        print('---------------')
     print(ans)
 
 
-def bfs(sPos, town, hList, mid, N):
-    y, x = sPos[0], sPos[1]
-    visit = [[0 for j in range(N)] for i in range(N)]
-    visit[y][x] = 1
+def dfs(y, x, visit, maxH, minH, hBoard, mid, N, KList):
     dy = [-1, -1, -1, 0, 0, 1, 1, 1]
     dx = [-1, 0, 1, -1, 1, -1, 0, 1]
-    q = deque()
-    q.append([y, x])
-    minH, maxH = 1000001, 0
-    while len(q) > 0:
-        tmp = q.popleft()
-        y, x = tmp[0], tmp[1]
-        curH = hList[y][x]
-        flag = 1
-        if curH > maxH:
-            if curH - minH > mid:
-                visit[y][x] = -1
-                flag = 0
-        if curH <= minH:
-            if maxH - curH > mid:
-                visit[y][x] = -1
-                flag = 0
-        if flag:
-            if curH >= maxH:
-                maxH = curH
-            if curH <= minH:
-                minH = curH
-            for i in range(8):
-                ny, nx = y + dy[i], x + dx[i]
-                if 0 <= ny < N and 0 <= nx < N and visit[ny][nx] == 0:
-                    q.append([ny, nx])
-                    visit[ny][nx] = 1
+    flag = 1
+    print('y, x, maxH, minH: %s, %s, %s, %s' %(y, x, maxH, minH))
     for i in range(N):
-        for j in range(N):
-            if town[i][j] == 'K' and visit[i][j] != 1:
-                return 0
-    return 1
+        print(visit[i])
+    for pos in KList:
+        y, x = pos[0], pos[1]
+        if visit[y][x] == 0:
+            flag = 0
+            break
+    if flag:
+        return 1
+    for i in range(8):
+        ny, nx = y + dy[i], x + dx[i]
+        if 0 <= ny < N and 0 <= nx < N and visit[ny][nx] == 0:
+            nxtH = hBoard[ny][nx]
+            print('ny, nx: %s, %s' %(ny, nx))
+            result = 0
+            if minH <= nxtH <= maxH:
+                visit[ny][nx] = 1
+                result = dfs(ny, nx, visit, maxH, minH, hBoard, mid, N, KList)
+                visit[ny][nx] = 0
+            if maxH < nxtH and nxtH - minH <= mid:
+                visit[ny][nx] = 1
+                result = dfs(ny, nx, visit, nxtH, minH, hBoard, mid, N, KList)
+                visit[ny][nx] = 0
+            if nxtH < minH and maxH - nxtH <= mid:
+                visit[ny][nx] = 1
+                result = dfs(ny, nx, visit, maxH, nxtH, hBoard, mid, N, KList)
+                visit[ny][nx] = 0
+            if result:
+                return 1
+    return 0
 
 
 if __name__ == "__main__":
