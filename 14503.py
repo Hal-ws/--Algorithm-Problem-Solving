d3c0d164b807b4a7b3ebb4f31728e0b57fee4443
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-10-21 21:41:21 +0900

    Update 14503.py

--- 14503.py
+++ 14503.py
@@ -3,67 +3,42 @@ import sys
 
 def main():
     N, M = map(int, sys.stdin.readline().split())
-    r, c, d= map(int, sys.stdin.readline().split())
+    r, c, d = map(int, sys.stdin.readline().split())
+    rY, rX = r, c
     room = []
-    robot = [r, c, d, 0, 1] # 위치(r, c) d(방향), 현 위치에서 청소 불가능한 인접 block 수, 청소한 블록수
     for i in range(N):
         room.append(list(map(int, sys.stdin.readline().split())))
-    room[r][c] = 9 # 시작지점 청소함(청소한 칸: 9)
+    dy = [-1, 0, 1, 0]
+    dx = [0, 1, 0, -1]
+    chkDy = [0, -1, 0, 1]
+    chkDx = [-1, 0, 1, 0]
+    rCnt = 0 # 회전횟수
+    cnt = 0
     while 1:
-        if acting(room, robot) == 0:
-            break
-    print(robot[4])
-
-
-def acting(room, robot):
-    robot[3] = 0
-    for i in range(4):
-        if robot[2] == 1:
-            if room[robot[0] - 1][robot[1]] == 0:
-                robot[2], robot[0], robot[4] = 0, robot[0] - 1, robot[4] + 1
-                break
-            else:
-                robot[2], robot[3] = 0, robot[3] + 1
-        elif robot[2] == 2:
-            if room[robot[0]][robot[1] + 1] == 0:
-                robot[2], robot[1], robot[4] = 1, robot[1] + 1, robot[4] + 1
-                break
-            else:
-                robot[2], robot[3] = 1, robot[3] + 1
-        elif robot[2] == 3:
-            if room[robot[0] + 1][robot[1]] == 0:
-                robot[2], robot[0], robot[4] = 2, robot[0] + 1, robot[4] + 1
-                break
-            else:
-                robot[2], robot[3] = 2, robot[3] + 1
-        elif robot[2] == 0:
-            if room[robot[0]][robot[1] - 1] == 0:
-                robot[2], robot[1], robot[4] = 3, robot[1] - 1, robot[4] + 1
-                break
-            else:
-                robot[2], robot[3] = 3, robot[3] + 1
-    room[robot[0]][robot[1]] = 9 # 움직인 부분 청소
-    if robot[3] == 4: ## 지금 위치에서 더 이상 청소할 데 없음. 후진 프로세스
-        if robot[2] == 1:
-            if room[robot[0]][robot[1] - 1] != 1: # 뒤로갈 공간 있음
-                robot[1] -= 1
-            else: #없음
-                return 0
-        if robot[2] == 2:
-            if room[robot[0] - 1][robot[1]] != 1:
-                robot[0] -= 1
-            else:
-                return 0
-        if robot[2] == 3:
-            if room[robot[0]][robot[1] + 1] != 1:
-                robot[1] += 1
-            else:
-                return 0
-        if robot[2] == 0:
-            if room[robot[0] + 1][robot[1]] != 1:
-                robot[0] += 1
-            else:
-                return 0
+        if room[rY][rX] == 0:  # 청소안한 곳이면 청소
+            room[rY][rX] = 8
+            cnt += 1
+        cY, cX = rY + chkDy[d], rX + chkDx[d]  # 왼쪽 칸 위치
+        d -= 1  # 회전
+        if d < 0:
+            d = 3
+        rCnt += 1
+        if room[cY][cX] == 0:
+            rY, rX = rY + dy[d], rX + dx[d]
+            rCnt = 0
+            continue
+        else:  # 왼쪽 방향에 청소할 공간 없음
+            if rCnt == 4: # 4방향 전부 다 청소공간 없음
+                bd = (d + 2) % 4  # 뒤를 바라보게 함
+                ny, nx = rY + dy[bd], rX + dx[bd]
+                if room[ny][nx] != 1:
+                    rY, rX = ny, nx
+                    rCnt = 0
+                    continue
+                else:
+                    print(cnt)
+                    break
+    return 0
 
 
 if __name__ == "__main__":
