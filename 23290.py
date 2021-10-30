55792861d4e140e7d6303fc2f61bd7d0c34f80ba
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-10-31 00:58:01 +0900

    Update 23290.py

--- 23290.py
+++ 23290.py
@@ -6,12 +6,14 @@ from copy import deepcopy
 
 def main():
     M, S = map(int, sys.stdin.readline().split())
-    paths = list(product([2, 0, 6, 4], repeat=3))
+    paths = list(product([0, 1, 2, 3], repeat=3))
     fBoard =[[[] for j in range(4)] for i in range(4)]
     eBoard = [[[] for j in range(4)] for i in range(4)]
     sBoard = [[deque() for j in range(4)] for i in range(4)]
     dy = [0, -1, -1, -1, 0, 1, 1, 1]
     dx = [-1, -1, 0, 1, 1, 1, 0, -1]
+    dsy = [-1, 0, 1, 0]
+    dsx = [0, -1, 0, 1]
     for _ in range(M):
         x, y, d = map(int, sys.stdin.readline().split())
         d -= 1
@@ -24,46 +26,43 @@ def main():
     sY -= 1
     for t in range(S):
         newBoard = [[[] for j in range(4)] for i in range(4)]
-        print('%sth try start' %t)
-        print('fBoard')
-        for i in range(4):
-            print(fBoard[i])
-        print('sY, sX: %s, %s' %(sY, sX))
         ## 복사마법
         for j in range(4):
             for i in range(4):
                 for d in fBoard[i][j]:
                     eBoard[i][j].append(d)
         ## 회전하고 이동
-        print('start rotation')
         for i in range(4):
             for j in range(4):
                 for d in fBoard[i][j]:
-                    print('y, x, d: %s, %s, %s' %(y, x, d))
+                    flag = 0
                     for k in range(8):
+                        ny, nx = i + dy[d], j + dx[d]
+                        if 0 <= ny < 4 and 0 <= nx < 4 and len(sBoard[ny][nx]) == 0 and (nx != sX or ny != sY):
+                            flag = 1
+                            break
                         d -= 1
                         if d == -1:
                             d = 7
-                        ny, nx = i + dy[d], j + dx[d]
-                        if 0 <= ny < 4 and 0 <= nx < 4 and len(sBoard[ny][nx]) == 0 and (nx != sX or ny != sY):
-                            print('ny, nx: %s, %s' %(ny, nx))
-                            break
-                    newBoard[ny][nx].append(d)
+                    if flag:
+                        newBoard[ny][nx].append(d)
+                    else:
+                        newBoard[i][j].append(d)
         fBoard = deepcopy(newBoard)
-        print('after move')
-        for i in range(4):
-            print(fBoard[i])
         ## 상어가 이동함
         candidates = []
         for path in paths:
             y, x = sY, sX
             flag = 1
             cnt = 0
+            visit = [[0 for j in range(4)] for i in range(4)]
             for d in path:
-                y = y + dy[d]
-                x = x + dx[d]
+                y = y + dsy[d]
+                x = x + dsx[d]
                 if 0 <= y < 4 and 0 <= x < 4:
-                    cnt += len(fBoard[y][x])
+                    if visit[y][x] == 0:
+                        cnt += len(fBoard[y][x])
+                    visit[y][x] = 1
                 else:
                     flag = 0
                     break
@@ -71,12 +70,12 @@ def main():
                 candidates.append([-cnt, path])
         candidates.sort()
         path = candidates[0][1]
-        print(candidates)
         for d in path:
-            sY += dy[d]
-            sX += dx[d]
+            sY += dsy[d]
+            sX += dsx[d]
+            if len(fBoard[sY][sX]) > 0:
+                sBoard[sY][sX].append(t)
             fBoard[sY][sX] = []
-            sBoard[sY][sX].append(t)
         ## 냄새 사라짐
         for i in range(4):
             for j in range(4):
@@ -87,6 +86,7 @@ def main():
             for j in range(4):
                 for d in eBoard[i][j]:
                     fBoard[i][j].append(d)
+                eBoard[i][j] = []
     ans = 0
     for i in range(4):
         for j in range(4):
